8257bd756a583dc2689c7f6ec586bc7144290460
Giorgio Sironi <g.sironi@elifesciences.org>
2016-06-06 10:53:51 +0100

    Removing commitFile after being done with it

--- vars/elifeGitRevision.groovy
+++ vars/elifeGitRevision.groovy
@@ -2,5 +2,6 @@ def call() {
     def commitFile = "${env.BUILD_TAG}.commit.txt"
     sh "git rev-parse HEAD > ${commitFile}"
     def commit = readFile commitFile
+    sh "rm ${commitFile}"
     return commit
 }
