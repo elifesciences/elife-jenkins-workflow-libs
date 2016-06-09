fcde2e82947978fb5dd608cf7fe9cc18681ab68a
Giorgio Sironi <g.sironi@elifesciences.org>
2016-06-09 15:21:58 +0100

    Trying to save test artifact in elifeEnd2EndTest

--- vars/elifeEnd2EndTest.groovy
+++ vars/elifeEnd2EndTest.groovy
@@ -1,5 +1,6 @@
 def call() {
     node("end2end") {
         sh "cd ${env.SPECTRUM_PREFIX}; sudo -H -u elife ${env.SPECTRUM_PREFIX}execute.sh"
+        step([$class: "JUnitResultArchiver", testResults: "${env.SPECTRUM_PREFIX}build/junit.xml"])
     }
 }
