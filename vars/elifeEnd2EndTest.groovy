52e30edfee3913d40921c8d0e1e9bc9d334ae66f
Giorgio Sironi <g.sironi@elifesciences.org>
2016-06-10 09:07:13 +0100

    Moving end2end test artifact in workspace

--- vars/elifeEnd2EndTest.groovy
+++ vars/elifeEnd2EndTest.groovy
@@ -1,6 +1,8 @@
 def call() {
     node("end2end") {
         sh "cd ${env.SPECTRUM_PREFIX}; sudo -H -u elife ${env.SPECTRUM_PREFIX}execute.sh"
-        step([$class: "JUnitResultArchiver", testResults: "${env.SPECTRUM_PREFIX}build/junit.xml"])
+        def end2endTestArtifact = "${env.BUILD_TAG}.end2end.junit.xml"
+        sh "cp ${env.SPECTRUM_PREFIX}build/junit.xml ${end2endTestArtifact}"
+        step([$class: "JUnitResultArchiver", testResults: end2endTestArtifact])
     }
 }
