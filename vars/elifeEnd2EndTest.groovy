c2baee7d7e067574055aa879b3a6142b3a31f4a8
Giorgio Sironi <g.sironi@elifesciences.org>
2016-06-10 11:02:31 +0100

    Archiving end2end test log

--- vars/elifeEnd2EndTest.groovy
+++ vars/elifeEnd2EndTest.groovy
@@ -1,8 +1,13 @@
 def call() {
     node("end2end") {
         sh "cd ${env.SPECTRUM_PREFIX}; sudo -H -u elife ${env.SPECTRUM_PREFIX}execute.sh"
-        def end2endTestArtifact = "${env.BUILD_TAG}.end2end.junit.xml"
-        sh "cp ${env.SPECTRUM_PREFIX}build/junit.xml ${end2endTestArtifact}"
+        
+        def end2endTestXmlArtifact = "${env.BUILD_TAG}.end2end.junit.xml"
+        sh "cp ${env.SPECTRUM_PREFIX}build/junit.xml ${end2endTestXmlArtifact}"
         step([$class: "JUnitResultArchiver", testResults: end2endTestArtifact])
+
+        def end2endTestLogArtifact = "${env.BUILD_TAG}.end2end.log"
+        sh "cp ${env.SPECTRUM_PREFIX}build/test.log ${end2endTestLogArtifact}"
+        archive $end2endTestLogArtifact
     }
 }
