1b7b34bac47ad8471e82ff8a2eb2ef973ecf0bf0
Giorgio Sironi <g.sironi@elifesciences.org>
2016-06-21 09:42:43 +0100

    elifeEnd2EndTest archives artifacts also in failure cases

--- vars/elifeEnd2EndTest.groovy
+++ vars/elifeEnd2EndTest.groovy
@@ -1,6 +1,6 @@
 def call() {
     node("end2end") {
-        sh "cd ${env.SPECTRUM_PREFIX}; sudo -H -u elife ${env.SPECTRUM_PREFIX}execute.sh"
+        sh "cd ${env.SPECTRUM_PREFIX}; sudo -H -u elife ${env.SPECTRUM_PREFIX}execute.sh || echo TESTS FAILED"
         
         def end2endTestXmlArtifact = "${env.BUILD_TAG}.end2end.junit.xml"
         sh "cp ${env.SPECTRUM_PREFIX}build/junit.xml ${end2endTestXmlArtifact}"
@@ -9,5 +9,7 @@ def call() {
         def end2endTestLogArtifact = "${env.BUILD_TAG}.end2end.log"
         sh "cp ${env.SPECTRUM_PREFIX}build/test.log ${end2endTestLogArtifact}"
         archive end2endTestLogArtifact
+
+        elifeVerifyJunitXml end2endTestXmlArtifact
     }
 }
