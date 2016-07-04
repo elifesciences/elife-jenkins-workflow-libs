b00bf16488dbc921533d444c15d9ae1e961560ea
Giorgio Sironi <g.sironi@elifesciences.org>
2016-07-04 11:41:24 +0100

    Added builderProjectTests()

---
+++ vars/builderProjectTests.groovy
@@ -0,0 +1,9 @@
+def call(stackname, folder, propagateFailure=false) {
+    def projectTestsCmd = "cd ${folder}; ./project_tests.sh"
+    if (propagateFailure) {
+        projectTestsCmd = projectTestsCmd + ' || echo TESTS_FAILED'
+    }
+    builderprojectTestsCmd stackname, projectTestsCmd
+    def smokeTestsCmd = "cd ${folder}; test -e smoke_tests.sh && ./smoke_tests.sh || echo 'No smoke tests to execute'"
+    builderCmd stackname, smokeTestsCmd
+}
