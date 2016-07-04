57fba3d17f6ed2a1497873d1483deb2201629e10
Giorgio Sironi <g.sironi@elifesciences.org>
2016-07-04 13:51:56 +0100

    No need to have a temporaryLocalTestArtifact since builder runs as jenkins user

--- vars/builderTestArtifact.groovy
+++ vars/builderTestArtifact.groovy
@@ -3,8 +3,6 @@ def call(localTestArtifact, stackname=null, remoteTestArtifact = null) {
         if (remoteTestArtifact == null) {
             throw new Exception("When specifying a `stackname`, you must also specify a `remoteTestArtifact` to retrieve");
         }
-        sh "touch ${temporarylocalTestArtifact}"
-        sh "chmod 666 ${temporarylocalTestArtifact}"
         sh "sudo -H -u elife ${env.BUILDER_PATH}bldr download_file:${stackname},${remoteTestArtifact},${localTestArtifact}"
     }
     step([$class: "JUnitResultArchiver", testResults: localTestArtifact])
