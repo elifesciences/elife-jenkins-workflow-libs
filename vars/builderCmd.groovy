b5cae2c5edaa5a50f60db1084828996d2d257c10
Giorgio Sironi <g.sironi@elifesciences.org>
2016-07-04 13:49:59 +0100

    The one true way of escaping single quotes: don't

--- vars/builderCmd.groovy
+++ vars/builderCmd.groovy
@@ -1,6 +1,9 @@
 def call(stackname, cmd) {
     cmd = cmd.replaceAll('=', '\\=')
-    cmd = cmd.replaceAll(/'/, "\\\\'")
+    def closedQuote = "'"
+    def quote = "\\\\'"
+    def openQuote = "'"
+    cmd = cmd.replaceAll(/'/, closedQuote + quote + openQuote)
     def shellCmd = "${env.BUILDER_PATH}bldr 'cmd:${stackname},${cmd}'"
     print "About to execute: ${shellCmd}"
     sh shellCmd 
