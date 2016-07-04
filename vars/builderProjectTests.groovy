5e8b7036a3773b72fe6abd03e5afa54f330e9e33
Giorgio Sironi <g.sironi@elifesciences.org>
2016-07-04 13:11:13 +0100

    Avoiding usage of forbidden +

--- vars/builderProjectTests.groovy
+++ vars/builderProjectTests.groovy
@@ -1,7 +1,7 @@
 def call(stackname, folder, propagateFailure=false) {
     def projectTestsCmd = "cd ${folder}; ./project_tests.sh"
     if (!propagateFailure) {
-        projectTestsCmd = projectTestsCmd + ' || echo TESTS_FAILED'
+        projectTestsCmd = "${projectTestsCmd} || echo TESTS_FAILED"
     }
     builderCmd stackname, projectTestsCmd
     def smokeTestsCmd = "cd ${folder}; test -e smoke_tests.sh && ./smoke_tests.sh || echo 'No smoke tests to execute'"
