d6dd2978bd419e3710b369d7e81ea82fb41933a6
Giorgio Sironi <g.sironi@elifesciences.org>
2016-07-04 13:35:49 +0100

    Debugging builderCmd

--- vars/builderCmd.groovy
+++ vars/builderCmd.groovy
@@ -1,5 +1,6 @@
 def call(stackname, cmd) {
     cmd = cmd.replaceAll('=', '\\=')
     cmd = cmd.replaceAll(/'/, "\\\\'")
+    print "About to execute: ${cmd}"
     sh "${env.BUILDER_PATH}bldr 'cmd:${stackname},${cmd}'"
 }
--- vars/builderProjectTests.groovy
+++ vars/builderProjectTests.groovy
@@ -4,6 +4,7 @@ def call(stackname, folder, propagateFailure=false) {
         projectTestsCmd = "${projectTestsCmd} || echo TESTS_FAILED"
     }
     builderCmd stackname, projectTestsCmd
+
     def smokeTestsCmd = "cd ${folder}; test -e smoke_tests.sh && ./smoke_tests.sh || echo 'No smoke tests to execute'"
     builderCmd stackname, smokeTestsCmd
 }
