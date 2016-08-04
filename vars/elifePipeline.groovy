7302f1ee2ee06432e440e5b46b071de6cdae215c
Giorgio Sironi <g.sironi@elifesciences.org>
2016-08-04 08:58:04 +0100

    Some methods are not CPS-compilable yet

--- vars/elifePipeline.groovy
+++ vars/elifePipeline.groovy
@@ -1,18 +1,29 @@
+import com.cloudbees.groovy.cps.NonCPS
+
+def findMaintainers(fileName) {
+    if (fileExists(fileName)) {
+        maintainersFile = readFile fileName
+        maintainers = maintainersFile.tokenize("\n").collect { it.trim() }
+        return maintainers
+    }
+
+    return []
+}
+
 def call(Closure body) {
+    def 
     node {
         timestamps {
             wrap([$class: 'AnsiColorBuildWrapper']) {
                 try {
                     body()
                 } catch (e) {
-                    if (fileExists('maintainers.txt')) {
-                        maintainersFile = readFile 'maintainers.txt'
-                        maintainers = maintainersFile.tokenize("\n").collect { it.trim() }
-                        def w = new StringWriter()
-                        e.printStackTrace(new PrintWriter(w))
-                        maintainers.each { address ->
-                            mail subject: "failed with ${e.message}", to: address, body: "Failed: ${w}"
-                        }
+                    def w = new StringWriter()
+                    e.printStackTrace(new PrintWriter(w))
+                    maintainers = findMaintainers 'maintainers.txt'
+                    for (int i = 0; i < maintainers.size(); i++) {
+                        def address = maintainers.get(i)
+                        mail subject: "failed with ${e.message}", to: address, body: "Failed: ${w}"
                     }
                     throw e
                 } finally {
