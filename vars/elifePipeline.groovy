36ab80198c35f2cb6a0611d3d84cee047307ff67
Giorgio Sironi <g.sironi@elifesciences.org>
2016-08-04 08:48:53 +0100

    elifePipeline sends mail on failure to maintainers.txt

--- vars/elifePipeline.groovy
+++ vars/elifePipeline.groovy
@@ -2,8 +2,22 @@ def call(Closure body) {
     node {
         timestamps {
             wrap([$class: 'AnsiColorBuildWrapper']) {
-                body()
-                deleteDir()
+                try {
+                    body()
+                } catch (e) {
+                    if (fileExists('maintainers.txt')) {
+                        maintainersFile = readFile 'maintainers.txt'
+                        maintainers = maintainersFile.tokenize("\n").collect { it.trim() }
+                        def w = new StringWriter()
+                        e.printStackTrace(new PrintWriter(w))
+                        maintainers.each { address ->
+                            mail subject: "failed with ${e.message}", to: address, body: "Failed: ${w}"
+                        }
+                    }
+                    throw e
+                } finally {
+                    deleteDir()
+                }
             }
         }
     }
