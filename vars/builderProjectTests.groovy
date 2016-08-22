e6ce84b1f5d76b73f02f1468ed85844663be55dd
Giorgio Sironi <g.sironi@elifesciences.org>
2016-08-22 12:33:15 +0100

    Trying to always retrieve artifacts, with finally

--- vars/builderProjectTests.groovy
+++ vars/builderProjectTests.groovy
@@ -1,24 +1,27 @@
+def retrieveArtifacts(stackname, testArtifacts) {
+    def localTestArtifacts = []
+    for (int i = 0; i < testArtifacts.size(); i++) {
+        def remoteTestArtifact = testArtifacts.get(i)
+        def slash = remoteTestArtifact.lastIndexOf('/')
+        def basename = remoteTestArtifact[slash+1..-1]
+        def localTestArtifact = "${env.BUILD_TAG}.${basename}"
+        localTestArtifacts << localTestArtifact
+        builderTestArtifact localTestArtifact, stackname, remoteTestArtifact, true
+    }
+    for (int i = 0; i < localTestArtifacts.size(); i++) {
+        def localTestArtifact = localTestArtifacts.get(i)
+        if (fileExists(localTestArtifact)) {
+            elifeVerifyJunitXml localTestArtifact
+        }
+    }
+}
+
 def call(stackname, folder, testArtifacts=[]) {
     def projectTestsCmd = "cd ${folder}; ./project_tests.sh"
     try {
         builderCmd stackname, projectTestsCmd
-    } catch (e) {
-        def localTestArtifacts = []
-        for (int i = 0; i < testArtifacts.size(); i++) {
-            def remoteTestArtifact = testArtifacts.get(i)
-            def slash = remoteTestArtifact.lastIndexOf('/')
-            def basename = remoteTestArtifact[slash+1..-1]
-            def localTestArtifact = "${env.BUILD_TAG}.${basename}"
-            localTestArtifacts << localTestArtifact
-            builderTestArtifact localTestArtifact, stackname, remoteTestArtifact, true
-        }
-        for (int i = 0; i < localTestArtifacts.size(); i++) {
-            def localTestArtifact = localTestArtifacts.get(i)
-            if (fileExists(localTestArtifact)) {
-                elifeVerifyJunitXml localTestArtifact
-            }
-        }
-        throw e
+    } finally {
+        retrieveArtifacts stackname, testArtifacts
     }
     
     builderSmokeTests stackname, folder
