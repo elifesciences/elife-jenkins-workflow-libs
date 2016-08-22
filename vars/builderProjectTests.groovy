12807ce44e090f5f8d04b78c833125fe465419b7
Giorgio Sironi <g.sironi@elifesciences.org>
2016-08-22 11:32:56 +0100

    Allow missing test artifacts
    May be related to tests that didn't run because of an earlier failure, and we don't want to obscure the result with their misleading error

--- vars/builderProjectTests.groovy
+++ vars/builderProjectTests.groovy
@@ -10,7 +10,7 @@ def call(stackname, folder, testArtifacts=[]) {
             def basename = remoteTestArtifact[slash+1..-1]
             def localTestArtifact = "${env.BUILD_TAG}.{$basename}"
             localTestArtifacts << localTestArtifact
-            builderTestArtifact localTestArtifact, stackname, remoteTestArtifact
+            builderTestArtifact localTestArtifact, stackname, remoteTestArtifact, true
         }
         for (int i = 0; i < localTestArtifacts; i++) {
             def localTestArtifact = localTestArtifacts.get(i)
