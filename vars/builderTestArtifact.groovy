21aaf81062d6dc17356514f6f2b94e6fda9e313d
Giorgio Sironi <g.sironi@elifesciences.org>
2016-08-22 15:07:22 +0100

    Trying to only use workspace-relative path of test artifact

--- vars/builderTestArtifact.groovy
+++ vars/builderTestArtifact.groovy
@@ -8,18 +8,22 @@ def call(localTestArtifact, stackname=null, remoteTestArtifact = null, allowMiss
         }
 
         def allowMissingParameter = allowMissing ? "True" : "False"
+        /*
         def localTestArtifactFullPath = "${env.WORKSPACE}/${localTestArtifact}"
         echo "Creating empty ${localTestArtifactFullPath}"
         sh "touch ${localTestArtifactFullPath}"
-        echo "Downloading onto ${localTestArtifactFullPath}"
-        sh "${env.BUILDER_PATH}bldr download_file:${stackname},${remoteTestArtifact},${localTestArtifactFullPath},allow_missing=${allowMissingParameter}"
+        */
+        echo "Creating empty ${localTestArtifact}"
+        sh "touch ${localTestArtifact}"
+        echo "Downloading onto ${localTestArtifact}"
+        sh "${env.BUILDER_PATH}bldr download_file:${stackname},${remoteTestArtifact},${localTestArtifact},allow_missing=${allowMissingParameter}"
     }
-    if (!readFile(localTestArtifactFullPath)) {
+    if (!readFile(localTestArtifact)) {
         if (allowMissing) {
-            echo "Artifact ${localTestArtifactFullPath} not found, but was configured to ignore missing artifacts."
+            echo "Artifact ${localTestArtifact} not found, but was configured to ignore missing artifacts."
             return
         } else {
-            error "Tests failed without leaving around an artifact at ${remoteTestArtifact}, which should have been downloaded at ${localTestArtifactFullPath}."
+            error "Tests failed without leaving around an artifact at ${remoteTestArtifact}, which should have been downloaded at ${localTestArtifact}."
         }
     }
     echo "Found ${localTestArtifact}"
