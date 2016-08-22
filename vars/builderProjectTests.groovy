d8fd82b327009e5bc42d11251cb4dc5b7900100a
Giorgio Sironi <g.sironi@elifesciences.org>
2016-08-22 11:31:37 +0100

    Verifying test artifacts after collection of all of them

--- vars/builderProjectTests.groovy
+++ vars/builderProjectTests.groovy
@@ -3,12 +3,18 @@ def call(stackname, folder, testArtifacts=[]) {
     try {
         builderCmd stackname, projectTestsCmd
     } catch (e) {
+        def localTestArtifacts = []
         for (int i = 0; i < testArtifacts.size(); i++) {
             def remoteTestArtifact = testArtifacts.get(i)
             def slash = remoteTestArtifact.lastIndexOf('/')
             def basename = remoteTestArtifact[slash+1..-1]
             def localTestArtifact = "${env.BUILD_TAG}.{$basename}"
+            localTestArtifacts << localTestArtifact
             builderTestArtifact localTestArtifact, stackname, remoteTestArtifact
+        }
+        for (int i = 0; i < localTestArtifacts; i++) {
+            def localTestArtifact = localTestArtifacts.get(i)
+            elifeVerifyJunitXml localTestArtifact
         }
         throw e
     }
