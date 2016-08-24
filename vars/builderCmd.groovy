4475cb27fd796abcd6da13eec8e895b91a745449
Giorgio Sironi <g.sironi@elifesciences.org>
2016-08-24 14:16:41 +0100

    Backslashing also commas in builderCmd

--- vars/builderCmd.groovy
+++ vars/builderCmd.groovy
@@ -1,9 +1,18 @@
-def call(stackname, cmd) {
-    cmd = cmd.replaceAll('=', '\\=')
+def backslash(character, string) {
+    return string.replaceAll(character, '\\' + character)
+}
+
+def backslashQuotes(string) {
     def closedQuote = "'"
     def quote = "\\\\'"
     def openQuote = "'"
-    cmd = cmd.replaceAll(/'/, closedQuote + quote + openQuote)
+    return string.replaceAll(/'/, closedQuote + quote + openQuote)
+}
+
+def call(stackname, cmd) {
+    cmd = backslash('=', cmd)
+    cmd = backslash(',', cmd)
+    cmd = backslashQuotes(cmd)
     def shellCmd = "${env.BUILDER_PATH}bldr 'cmd:${stackname},${cmd}'"
     print "About to execute: ${shellCmd}"
     sh shellCmd 
