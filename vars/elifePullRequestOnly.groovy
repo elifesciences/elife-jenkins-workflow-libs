f1811533e04c26e169a85b0934e204ac521235fe
Giorgio Sironi <g.sironi@elifesciences.org>
2016-09-01 09:49:38 +0100

    Added elifePullRequestOnly()

--- vars/elifePullRequestOnly.groovy
+++ vars/elifePullRequestOnly.groovy
@@ -1,9 +1,9 @@
 def call(Closure body) {
     // env.BRANCH_NAME is only specified for multibranch projects
     // and by extension, organization folders
-    if (env.BRANCH_NAME == null) {
+    if (env.BRANCH_NAME != null) {
         body()
     } else {
-        echo "Skipped actions because not on the mainline"
+        echo "Skipped actions because not on a pull request"
     }
 }
