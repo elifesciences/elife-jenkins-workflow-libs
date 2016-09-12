0ed027698a8edc8831652433e48e74164c0d30ee
Giorgio Sironi <g.sironi@elifesciences.org>
2016-09-12 09:03:49 +0100

    Removing old builder Groovy steps

--- vars/elifeCmd.groovy
+++
@@ -1,3 +0,0 @@
-def call(stackname, cmd) {
-    sh "sudo -H -u elife ${env.OLD_BUILDER_SCRIPTS_PREFIX}cmd '${stackname},${cmd}'"
-}
--- vars/elifeSwitchRevision.groovy
+++
@@ -1,4 +0,0 @@
-// TODO: rename in elifeDeploy
-def call(stackname, commit) {
-    sh "sudo -H -u elife ${env.OLD_BUILDER_SCRIPTS_PREFIX}switch_revision_update_instance ${stackname},${commit}"
-}
