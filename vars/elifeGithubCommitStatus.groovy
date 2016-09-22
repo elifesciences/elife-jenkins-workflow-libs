d1f2e3ea1b54873cae18aa80a2c07a0b562597f6
Giorgio Sironi <g.sironi@elifesciences.org>
2016-09-22 12:34:37 +0100

    description may contain spaces

--- vars/elifeGithubCommitStatus.groovy
+++ vars/elifeGithubCommitStatus.groovy
@@ -1,3 +1,3 @@
 def call(commit, status, context, description='elifeGithubCommitStatus step') {
-    sh "commit=${commit} status=${status} context=${context} description=${description} /usr/local/jenkins-scripts/notify_github_commit_status.sh"
+    sh "commit=${commit} status=${status} context=${context} description='${description}' /usr/local/jenkins-scripts/notify_github_commit_status.sh"
 }
