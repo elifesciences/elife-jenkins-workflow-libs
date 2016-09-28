61739b81510d1207d147dc7f158bd40b1ad8e1f4
Giorgio Sironi <g.sironi@elifesciences.org>
2016-09-28 11:32:56 +0100

    Using force in auto commits, to get also files that normally would be ignored

--- vars/elifeGitAutoCommit.groovy
+++ vars/elifeGitAutoCommit.groovy
@@ -1,4 +1,4 @@
 def call(message = "Automatic commit", add=".") {
-    sh "git add --all ${add}"
+    sh "git add --all --force ${add}"
     sh "git diff --quiet --exit-code --cached || git commit -m \"${message}\""
 }
