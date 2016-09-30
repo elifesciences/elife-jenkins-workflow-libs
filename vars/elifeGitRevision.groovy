6dc0d05a3a0346306bcae2a43194432fabc64774
Giorgio Sironi <g.sironi@elifesciences.org>
2016-09-30 14:07:45 +0100

    Simplifying with returnStdout option

--- vars/elifeGitRevision.groovy
+++ vars/elifeGitRevision.groovy
@@ -1,8 +1,3 @@
 def call() {
-    def commitFile = "${env.BUILD_TAG}.commit.txt"
-    sh "git rev-parse HEAD > ${commitFile}"
-    def commitFileContent = readFile commitFile
-    def commit = commitFileContent.trim()
-    sh "rm ${commitFile}"
-    return commit
+    return sh("git rev-parse HEAD > ${commitFile}", returnStdout=true).trim()
 }
