960c537c2a8ecd09a58ae56836682be9a123b8b7
Giorgio Sironi <g.sironi@elifesciences.org>
2016-10-03 15:38:02 +0100

    It is builderDeployRevision that should make sure a stack is running
    Much better place than builderProjectTests(), as we may want to only deploy and run smoke tests. Also works for part of other environments, like a single stack of end2end

--- vars/builderDeployRevision.groovy
+++ vars/builderDeployRevision.groovy
@@ -1,3 +1,4 @@
 def call(stackname, revision) {
+    builderStart stackname
     sh "${env.BUILDER_PATH}bldr 'switch_revision_update_instance:${stackname},${revision}'"
 }
--- vars/builderProjectTests.groovy
+++ vars/builderProjectTests.groovy
@@ -25,7 +25,6 @@ def retrieveArtifacts(stackname, testArtifacts) {
 def call(stackname, folder, testArtifacts=[]) {
     def projectTestsCmd = "cd ${folder}; ./project_tests.sh"
     try {
-        builderStart stackname
         builderCmd stackname, projectTestsCmd
     } finally {
         retrieveArtifacts stackname, testArtifacts
