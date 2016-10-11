8b7ae89012aa9db2ddef418baa64d8b95f15fcc2
Giorgio Sironi <g.sironi@elifesciences.org>
2016-10-11 09:31:28 +0100

    Extended elifeEnd2EndStep() with environmentName parameter

--- vars/elifeEnd2EndTest.groovy
+++ vars/elifeEnd2EndTest.groovy
@@ -1,6 +1,8 @@
-def call(Closure preliminaryStep=null, marker=null) {
-    lock('end2end') {
-        builderStartAll(elifeEnd2endStacks())
+def call(Closure preliminaryStep=null, marker=null, environmentName='end2end') {
+    lock(environment) {
+        if (environmentName == 'end2end') {
+            builderStartAll(elifeEnd2endStacks())
+        }
         if (preliminaryStep != null) {
             preliminaryStep()
         }
@@ -12,14 +14,14 @@ def call(Closure preliminaryStep=null, marker=null) {
 
         sh "cd ${env.SPECTRUM_PREFIX}; sudo -H -u elife ${env.SPECTRUM_PREFIX}execute.sh ${additionalArguments} || echo TESTS FAILED"
         
-        def end2endTestXmlArtifact = "${env.BUILD_TAG}.end2end.junit.xml"
-        sh "cp ${env.SPECTRUM_PREFIX}build/junit.xml ${end2endTestXmlArtifact}"
-        builderTestArtifact end2endTestXmlArtifact
+        def testXmlArtifact = "${env.BUILD_TAG}.${environmentName}.junit.xml"
+        sh "cp ${env.SPECTRUM_PREFIX}build/junit.xml ${testXmlArtifact}"
+        builderTestArtifact testXmlArtifact
 
-        def end2endTestLogArtifact = "${env.BUILD_TAG}.end2end.log"
-        sh "cp ${env.SPECTRUM_PREFIX}build/test.log ${end2endTestLogArtifact}"
-        archive end2endTestLogArtifact
+        def testLogArtifact = "${env.BUILD_TAG}.${environmentName}.log"
+        sh "cp ${env.SPECTRUM_PREFIX}build/test.log ${testLogArtifact}"
+        archive testLogArtifact
 
-        elifeVerifyJunitXml end2endTestXmlArtifact
+        elifeVerifyJunitXml testXmlArtifact
     }
 }
