b8ece43be582163d70d6cb09561c2530a026c5b2
Giorgio Sironi <g.sironi@elifesciences.org>
2016-10-11 09:33:15 +0100

    Renamed step with correct capitalization of `end2end`

--- vars/elifeEnd2EndTest.groovy
+++ vars/elifeEnd2EndTest.groovy
@@ -1,27 +1,3 @@
 def call(Closure preliminaryStep=null, marker=null, environmentName='end2end') {
-    lock(environment) {
-        if (environmentName == 'end2end') {
-            builderStartAll(elifeEnd2endStacks())
-        }
-        if (preliminaryStep != null) {
-            preliminaryStep()
-        }
-
-        def additionalArguments = ''
-        if (marker) {
-            additionalArguments = additionalArguments + "-m ${marker}"
-        }
-
-        sh "cd ${env.SPECTRUM_PREFIX}; sudo -H -u elife ${env.SPECTRUM_PREFIX}execute.sh ${additionalArguments} || echo TESTS FAILED"
-        
-        def testXmlArtifact = "${env.BUILD_TAG}.${environmentName}.junit.xml"
-        sh "cp ${env.SPECTRUM_PREFIX}build/junit.xml ${testXmlArtifact}"
-        builderTestArtifact testXmlArtifact
-
-        def testLogArtifact = "${env.BUILD_TAG}.${environmentName}.log"
-        sh "cp ${env.SPECTRUM_PREFIX}build/test.log ${testLogArtifact}"
-        archive testLogArtifact
-
-        elifeVerifyJunitXml testXmlArtifact
-    }
+    elifeEnd2endTest(preliminaryStep, marker, environmentName)
 }
