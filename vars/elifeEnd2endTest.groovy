29f9644fbf843cba223d76ecc1606ab5acc1d5af
Giorgio Sironi <g.sironi@elifesciences.org>
2016-10-11 09:42:50 +0100

    Correctly passing environment to elife-spectrum tests

--- vars/elifeEnd2endTest.groovy
+++ vars/elifeEnd2endTest.groovy
@@ -12,7 +12,7 @@ def call(Closure preliminaryStep=null, marker=null, environmentName='end2end') {
             additionalArguments = additionalArguments + "-m ${marker}"
         }
 
-        sh "cd ${env.SPECTRUM_PREFIX}; sudo -H -u elife ${env.SPECTRUM_PREFIX}execute.sh ${additionalArguments} || echo TESTS FAILED"
+        sh "cd ${env.SPECTRUM_PREFIX}; SPECTRUM_ENVIRONMENT=${environmentName} sudo -H -u elife ${env.SPECTRUM_PREFIX}execute.sh ${additionalArguments} || echo TESTS FAILED"
         
         def testXmlArtifact = "${env.BUILD_TAG}.${environmentName}.junit.xml"
         sh "cp ${env.SPECTRUM_PREFIX}build/junit.xml ${testXmlArtifact}"
