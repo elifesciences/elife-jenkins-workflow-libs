aa0b8ad6befadc3c15271c5e51c0d9e91841c556
Giorgio Sironi <g.sironi@elifesciences.org>
2016-11-14 15:47:07 +0000

    Making sure libraries-runner is connected after possibly starting it

--- vars/elifeLibrary.groovy
+++ vars/elifeLibrary.groovy
@@ -2,6 +2,7 @@ def call(Closure body) {
     elifePipeline {
         lock("elife-libraries--ci") {
             builderStart "elife-libraries--ci"
+            jenkinsCli "connect-node libraries-runner"
             node("libraries") {
                 body()
                 deleteDir()
