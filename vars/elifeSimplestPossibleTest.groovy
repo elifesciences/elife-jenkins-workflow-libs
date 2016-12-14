7e30a1512cf538ae0ccbe6285c4d09ec606ba5f2
Giorgio Sironi <g.sironi@elifesciences.org>
2016-12-14 16:13:39 +0000

    Added elifeSimplestPossibleTest

---
+++ vars/elifeSimplestPossibleTest.groovy
@@ -0,0 +1,5 @@
+def call(environment='continuumtest') {
+    lock(environment) {
+        sh "cd ${env.SPECTRUM_PREFIX}; SPECTRUM_ENVIRONMENT=${environment} sudo -H -u elife ${env.SPECTRUM_PREFIX}execute.sh"
+    }
+}
