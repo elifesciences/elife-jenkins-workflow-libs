ef7d7f992b5555b6e4bbb4c46186f872e934f6a6
Giorgio Sironi <g.sironi@elifesciences.org>
2017-01-20 15:17:21 +0000

    Allowing customization of base branch in elifeGithubPullRequest()

--- vars/elifeGithubPullRequest.groovy
+++ vars/elifeGithubPullRequest.groovy
@@ -1,7 +1,7 @@
-def call(branch, title, description = '', temporaryFile = 'pull-request.log') {
+def call(branch, title, description = '', base='origin/master', temporaryFile = 'pull-request.log') {
     sh "git push origin ${branch}"
     def fullText = title + "\n\n" + description
     writeFile(file: temporaryFile, text: fullText)
-    sh "hub pull-request -F ${temporaryFile} -h ${branch}"
+    sh "hub pull-request -F ${temporaryFile} -b ${base} -h ${branch}"
     sh "rm ${temporaryFile}"
 }
