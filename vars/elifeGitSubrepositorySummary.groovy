c3067cc3f5b902ff63da9f55ad1ea0af83181dda
Giorgio Sironi <g.sironi@elifesciences.org>
2017-01-24 14:44:15 +0000

    Removing pull request references from commit messages used as summaries

--- vars/elifeGitSubrepositorySummary.groovy
+++ vars/elifeGitSubrepositorySummary.groovy
@@ -1,5 +1,6 @@
 def call(folder) {
     def commit = sh(script: "cd ${folder}; git rev-parse HEAD", returnStdout: true).trim().substring(0, 7)
-    def commitMessage = sh(script: "cd ${folder}; git log -1 --pretty='%B' | head -n 1", returnStdout: true).trim()
+    def pullRequestReference = ~/ \(#\d+\)/
+    def commitMessage = sh(script: "cd ${folder}; git log -1 --pretty='%B' | head -n 1", returnStdout: true).trim() - pullRequestReference
     return "${commit}: ${commitMessage}"
 }
