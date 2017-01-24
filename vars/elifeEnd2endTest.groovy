9111096a0f57bb4e5aa68b6ae7621750be064703
Giorgio Sironi <g.sironi@elifesciences.org>
2017-01-24 09:17:17 +0000

    Cannot fetch yet as the repository is property of the elife user

--- vars/elifeEnd2endTest.groovy
+++ vars/elifeEnd2endTest.groovy
@@ -12,7 +12,7 @@ def call(Closure preliminaryStep=null, marker=null, environmentName='end2end', p
             additionalArguments = additionalArguments + "-m ${marker}"
         }
 
-        sh "cd ${env.SPECTRUM_PREFIX}; git fetch; git checkout ${revision}; SPECTRUM_ENVIRONMENT=${environmentName} SPECTRUM_PROCESSES=${processes} sudo -H -u elife ${env.SPECTRUM_PREFIX}execute.sh ${additionalArguments} || echo TESTS FAILED"
+        sh "cd ${env.SPECTRUM_PREFIX}; SPECTRUM_ENVIRONMENT=${environmentName} SPECTRUM_PROCESSES=${processes} sudo -H -u elife ${env.SPECTRUM_PREFIX}execute.sh ${additionalArguments} || echo TESTS FAILED"
         
         def testXmlArtifact = "${env.BUILD_TAG}.${environmentName}.junit.xml"
         sh "cp ${env.SPECTRUM_PREFIX}build/junit.xml ${testXmlArtifact}"
