712115d9b76cd8088054d51d2fe6e94d4e24a712
Giorgio Sironi <g.sironi@elifesciences.org>
2017-01-24 14:39:10 +0000

    Cutting sha1 commits to first 7 chars

--- vars/elifeGitSubrepositorySummary.groovy
+++ vars/elifeGitSubrepositorySummary.groovy
@@ -1,5 +1,5 @@
 def call(folder) {
-    def commit = sh(script: "cd ${folder}; git rev-parse HEAD", returnStdout: true).trim()
+    def commit = sh(script: "cd ${folder}; git rev-parse HEAD", returnStdout: true).trim().substring(0, 7)
     def commitMessage = sh(script: "cd ${folder}; git log -1 --pretty='%B' | head -n 1", returnStdout: true).trim()
     return "${commit}: ${commitMessage}"
 }
