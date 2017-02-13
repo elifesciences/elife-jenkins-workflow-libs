43d21549f7105da017b6aa19ae47c536fd3cf048
Giorgio Sironi <g.sironi@elifesciences.org>
2017-02-13 15:12:25 +0000

    Added elifeUpdatePipeline() generic, sort-of-declarative pipeline

---
+++ vars/elifeUpdatePipeline.groovy
@@ -0,0 +1,29 @@
+def call(Closure updateStep, Closure describeStep, branchPrefix='automated_jenkins_update_') {
+    elifePipeline {
+        def commit
+        def branch
+        stage 'Checkout and branch', {
+            checkout scm
+            commit = elifeGitRevision()
+            branch = elifeGitGenerateBranch branchPrefix
+        }
+
+        stage 'Update', updateStep
+
+        def differences
+        def shortDescription
+        stage 'Commit', {
+            differences = elifeGitDifferences()
+            elifeOnlyIf differences, {
+                shortDescription = describeStep()
+                elifeGitCommit shortDescription
+            }
+        }
+
+        stage 'Push and pull request', {
+            elifeOnlyIf differences, {
+                elifeGithubPullRequest branch, shortDescription, "I have run ${env.BUILD_URL} which resulted in this pull request.", 'develop'
+            }
+        }
+    }
+}
