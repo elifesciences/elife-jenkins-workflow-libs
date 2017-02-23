ccd6e62854be6dcb6bc1cf4066969ad7549f6ffa
Giorgio Sironi <g.sironi@elifesciences.org>
2017-02-23 14:43:41 +0000

    iiif--end2end added to list

--- vars/elifeEnd2endStacks.groovy
+++ vars/elifeEnd2endStacks.groovy
@@ -1,3 +1,3 @@
 def call() {
-    return ['api-gateway--end2end', 'elife-bot--end2end', 'elife-dashboard--end2end', 'elife-metrics--end2end', 'elife-website--end2end', 'journal--end2end', 'journal-cms--end2end', 'lax--end2end', 'medium--end2end', 'personalised-covers--end2end', 'recommendations--end2end', 'search--end2end']
+    return ['api-gateway--end2end', 'elife-bot--end2end', 'elife-dashboard--end2end', 'elife-metrics--end2end', 'elife-website--end2end', 'iiif--end2end', 'journal--end2end', 'journal-cms--end2end', 'lax--end2end', 'medium--end2end', 'personalised-covers--end2end', 'recommendations--end2end', 'search--end2end']
 }
