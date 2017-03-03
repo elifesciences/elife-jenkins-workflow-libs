97996bb579b6135779a5785c943a6a8e2b845a30
Giorgio Sironi <g.sironi@elifesciences.org>
2017-03-03 16:23:19 +0000

    Yet another additional parameter: articleId

--- vars/elifeEnd2endTest.groovy
+++ vars/elifeEnd2endTest.groovy
@@ -1,4 +1,6 @@
-def call(Closure preliminaryStep=null, marker=null, environmentName='end2end', processes=6, revision='master') {
+// TODO: reorder this horrible list of parameters
+// better: http://docs.groovy-lang.org/latest/html/documentation/#_named_arguments
+def call(Closure preliminaryStep=null, marker=null, environmentName='end2end', processes=6, revision='master', articleId=null) {
     lock(environmentName) {
         if (environmentName == 'end2end') {
             builderStartAll(elifeEnd2endStacks())
@@ -9,12 +11,15 @@ def call(Closure preliminaryStep=null, marker=null, environmentName='end2end', p
 
         def additionalArguments = ''
         if (marker) {
-            additionalArguments = additionalArguments + "-m ${marker}"
+            additionalArguments = additionalArguments + "-m ${marker} "
+        }
+        if (articleId) {
+            additionalArguments = additionalArguments + "--article-id=${articleId} "
         }
 
         sh "cd ${env.SPECTRUM_PREFIX}; sudo -H -u elife ${env.SPECTRUM_PREFIX}checkout.sh ${revision}"
         sh "cd ${env.SPECTRUM_PREFIX}; SPECTRUM_ENVIRONMENT=${environmentName} SPECTRUM_TIMEOUT=120 sudo -H -u elife ${env.SPECTRUM_PREFIX}execute-simplest-possible-test.sh"
-        sh "cd ${env.SPECTRUM_PREFIX}; SPECTRUM_ENVIRONMENT=${environmentName} SPECTRUM_PROCESSES=${processes} sudo -H -u elife ${env.SPECTRUM_PREFIX}execute.sh ${additionalArguments} || echo TESTS FAILED"
+        sh "cd ${env.SPECTRUM_PREFIX}; SPECTRUM_ENVIRONMENT=${environmentName} SPECTRUM_PROCESSES=${processes} sudo -H -u elife ${env.SPECTRUM_PREFIX}execute.sh ${additionalArguments}|| echo TESTS FAILED"
         
         def testXmlArtifact = "${env.BUILD_TAG}.${environmentName}.junit.xml"
         sh "cp ${env.SPECTRUM_PREFIX}build/junit.xml ${testXmlArtifact}"
