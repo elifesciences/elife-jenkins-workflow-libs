c7592e0433d861c682330abaef1b480e6d272725
Giorgio Sironi <g.sironi@elifesciences.org>
2017-03-28 14:14:23 +0100

    Additional lock on spectrum

--- vars/elifeSpectrum.groovy
+++ vars/elifeSpectrum.groovy
@@ -28,42 +28,44 @@ def call(Map parameters) {
     String revision = parameters.get('revision', 'master')
     String articleId = parameters.get('articleId')
 
-    lock(environmentName) {
-        if (environmentName == 'end2end') {
-            builderStartAll(elifeEnd2endStacks())
-        }
-        preliminaryStep()
-
-        try {
-            def additionalFilteringArguments = ''
-            if (marker) {
-                additionalFilteringArguments = additionalFilteringArguments + "-m ${marker} "
+    lock('spectrum') {
+        lock(environmentName) {
+            if (environmentName == 'end2end') {
+                builderStartAll(elifeEnd2endStacks())
             }
-            if (articleId) {
-                additionalFilteringArguments = additionalFilteringArguments + "--article-id=${articleId} "
+            preliminaryStep()
+
+            try {
+                def additionalFilteringArguments = ''
+                if (marker) {
+                    additionalFilteringArguments = additionalFilteringArguments + "-m ${marker} "
+                }
+                if (articleId) {
+                    additionalFilteringArguments = additionalFilteringArguments + "--article-id=${articleId} "
+                }
+
+                sh "cd ${env.SPECTRUM_PREFIX}; sudo -H -u elife ${env.SPECTRUM_PREFIX}checkout.sh ${revision}"
+                if (!additionalFilteringArguments) {
+                    // before starting the whole suite, run simple smoke test first
+                    sh "cd ${env.SPECTRUM_PREFIX}; SPECTRUM_ENVIRONMENT=${environmentName} SPECTRUM_TIMEOUT=120 sudo -H -u elife ${env.SPECTRUM_PREFIX}execute-simplest-possible-test.sh"
+                }
+                sh "cd ${env.SPECTRUM_PREFIX}; SPECTRUM_ENVIRONMENT=${environmentName} SPECTRUM_PROCESSES=${processes} sudo -H -u elife ${env.SPECTRUM_PREFIX}execute.sh ${additionalFilteringArguments}|| echo TESTS FAILED"
+                
+                def testXmlArtifact = "${env.BUILD_TAG}.${environmentName}.junit.xml"
+                sh "cp ${env.SPECTRUM_PREFIX}build/junit.xml ${testXmlArtifact}"
+                builderTestArtifact testXmlArtifact
+
+                def testLogArtifact = "${env.BUILD_TAG}.${environmentName}.log"
+                sh "cp ${env.SPECTRUM_PREFIX}build/test.log ${testLogArtifact}"
+                archive testLogArtifact
+
+                elifeVerifyJunitXml testXmlArtifact
+            } catch (e) {
+                echo "Failure while running spectrum tests: ${e.message}"
+                echo "Attempting to rollback (if the project specifies it) before terminating the build with an error"
+                rollbackStep()
+                throw e
             }
-
-            sh "cd ${env.SPECTRUM_PREFIX}; sudo -H -u elife ${env.SPECTRUM_PREFIX}checkout.sh ${revision}"
-            if (!additionalFilteringArguments) {
-                // before starting the whole suite, run simple smoke test first
-                sh "cd ${env.SPECTRUM_PREFIX}; SPECTRUM_ENVIRONMENT=${environmentName} SPECTRUM_TIMEOUT=120 sudo -H -u elife ${env.SPECTRUM_PREFIX}execute-simplest-possible-test.sh"
-            }
-            sh "cd ${env.SPECTRUM_PREFIX}; SPECTRUM_ENVIRONMENT=${environmentName} SPECTRUM_PROCESSES=${processes} sudo -H -u elife ${env.SPECTRUM_PREFIX}execute.sh ${additionalFilteringArguments}|| echo TESTS FAILED"
-            
-            def testXmlArtifact = "${env.BUILD_TAG}.${environmentName}.junit.xml"
-            sh "cp ${env.SPECTRUM_PREFIX}build/junit.xml ${testXmlArtifact}"
-            builderTestArtifact testXmlArtifact
-
-            def testLogArtifact = "${env.BUILD_TAG}.${environmentName}.log"
-            sh "cp ${env.SPECTRUM_PREFIX}build/test.log ${testLogArtifact}"
-            archive testLogArtifact
-
-            elifeVerifyJunitXml testXmlArtifact
-        } catch (e) {
-            echo "Failure while running spectrum tests: ${e.message}"
-            echo "Attempting to rollback (if the project specifies it) before terminating the build with an error"
-            rollbackStep()
-            throw e
         }
     }
 }
