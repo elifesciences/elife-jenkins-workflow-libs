de8c8bab22bf995a5ecbd2d66b24613cdc3be06f
Giorgio Sironi <g.sironi@elifesciences.org>
2017-04-04 09:30:06 +0100

    Printing original exit code of smoke tests

--- vars/builderSmokeTests.groovy
+++ vars/builderSmokeTests.groovy
@@ -1,4 +1,4 @@
 def call(stackname, folder) {
-    def smokeTestsCmd = "cd ${folder}; if [ -e smoke_tests.sh ]; then ./smoke_tests.sh; fi"
+    def smokeTestsCmd = "cd ${folder}; if [ -e smoke_tests.sh ]; then ./smoke_tests.sh; fi; code\\=$?; echo EXIT CODE: $code; exit $code"
     builderCmd stackname, smokeTestsCmd
 }
