1b8dea99bd846875c68f62adf45b4cec8666f269
Giorgio Sironi <g.sironi@elifesciences.org>
2017-04-11 13:10:17 +0100

    Adding iiif--ci to stacks to turn off

--- vars/elifeCiStacks.groovy
+++ vars/elifeCiStacks.groovy
@@ -1,3 +1,3 @@
 def call() {
-    return ['api-dummy--ci', 'elife-bot--ci', 'elife-dashboard--ci', 'elife-metrics--ci', 'journal--ci', 'journal-cms--ci', 'lax--ci', 'medium--ci', 'pattern-library--ci', 'personalised-covers--ci', 'recommendations--ci', 'reviewer-suggestions--ci', 'search--ci']
+    return ['api-dummy--ci', 'elife-bot--ci', 'elife-dashboard--ci', 'elife-metrics--ci', 'journal--ci', 'journal-cms--ci', 'iiif--ci', 'lax--ci', 'medium--ci', 'pattern-library--ci', 'personalised-covers--ci', 'recommendations--ci', 'reviewer-suggestions--ci', 'search--ci']
 }
