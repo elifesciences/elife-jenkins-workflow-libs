17e0b83c8961abd71448dc51ce6a3baabc8910ab
Giorgio Sironi <g.sironi@elifesciences.org>
2017-04-12 09:16:50 +0100

    Renovating elifeSimplestPossibleTest for consistency with elifeSpectrum

--- vars/elifeSimplestPossibleTest.groovy
+++ vars/elifeSimplestPossibleTest.groovy
@@ -1,5 +1,11 @@
 def call(environment='continuumtest') {
-    lock(environment) {
-        sh "cd ${env.SPECTRUM_PREFIX}; SPECTRUM_ENVIRONMENT=${environment} sudo -H -u elife ${env.SPECTRUM_PREFIX}execute-simplest-possible-test.sh"
+    lock('spectrum') {
+        lock(environment) {
+            if (environment == 'end2end') {
+                builderStartAll(elifeEnd2endStacks())
+            }
+            sh "cd ${env.SPECTRUM_PREFIX}; sudo -H -u elife ${env.SPECTRUM_PREFIX}checkout.sh origin/master"
+            sh "cd ${env.SPECTRUM_PREFIX}; SPECTRUM_ENVIRONMENT=${environment} sudo -H -u elife ${env.SPECTRUM_PREFIX}execute-simplest-possible-test.sh"
+        }
     }
 }
