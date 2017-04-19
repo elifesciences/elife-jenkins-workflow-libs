0924943efa08526c5a655956fa8e3b2a88824194
Giorgio Sironi <g.sironi@elifesciences.org>
2017-04-19 09:27:49 +0100

    auto commit has become a no-op

--- vars/elifeGitAutoCommit.groovy
+++ vars/elifeGitAutoCommit.groovy
@@ -1,10 +1,12 @@
 def call(message = "Automatic commit", add=".") {
     sh "git add --all --force ${add}"
     def code = sh script: "git diff --quiet --exit-code --cached", returnStatus: true
+    echo "git diff exit code: ${code}"
     if (code) {
-        "git commit -m \"${message}\""
+        sh "git commit -m \"${message}\""
         return true
     } else{
+        echo "No diff to commit"
         return false
     }
 }
