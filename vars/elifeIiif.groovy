d946dc56d8d2d5e934aa992e86c7c20e9a1f1ab2
Giorgio Sironi <g.sironi@elifesciences.org>
2017-04-25 15:41:28 +0100

    Adding what is essentially test--iiif and prod--iiif together

---
+++ vars/elifeIiif.groovy
@@ -0,0 +1,25 @@
+elifePipeline {
+    def commit
+    stage 'Checkout', {
+        git branch: 'develop', url: 'git://github.com/elifesciences/loris.git'
+        commit = elifeGitRevision()
+    }
+
+    stage 'Ci', {
+        lock('iiif--ci') {
+            builderDeployRevision 'iiif--ci', commit
+        }
+    }
+
+    stage 'End2end', {
+        lock('iiif--end2end') {
+            builderDeployRevision 'iiif--end2end', commit
+        }
+    }
+
+    stage 'Prod', {
+        lock('iiif--prod') {
+            builderDeployRevision 'iiif--prod', commit
+        }
+    }
+}
