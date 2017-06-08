39204849339653e95fb0fddce4b81460385a1991
Giorgio Sironi <g.sironi@elifesciences.org>
2017-06-08 10:58:25 +0100

    elifeNewRelicStatus() step

---
+++ vars/elifeNewRelicEnsureStatus.groovy
@@ -0,0 +1,8 @@
+def call(applicationId, allowedStatuses) {
+    def currentStatus = elifeNewRelicStatus(applicationId)
+    if (!allowedStatuses.contains(currentStatus)) {
+        throw new Exception("New Relic status for ${applicationId} was expected to be in ${allowedStatuses}, but it is `${currentStatus}`")
+    }
+}
+
+return this
