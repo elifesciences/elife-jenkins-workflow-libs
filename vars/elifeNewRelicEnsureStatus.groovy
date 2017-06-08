30f1a2f393bf730d5e58879d58201462c614373c
Giorgio Sironi <g.sironi@elifesciences.org>
2017-06-08 11:36:22 +0100

    Add positive log when check doesn't stop the build

--- vars/elifeNewRelicEnsureStatus.groovy
+++ vars/elifeNewRelicEnsureStatus.groovy
@@ -1,6 +1,8 @@
 def call(applicationId, allowedStatuses) {
     def currentStatus = elifeNewRelicStatus(applicationId)
-    if (!allowedStatuses.contains(currentStatus)) {
+    if (allowedStatuses.contains(currentStatus)) {
+        echo "New Relic status for ${applicationId} was expected to be in ${allowedStatuses}, it is `${currentStatus}`"
+    } else {
         throw new Exception("New Relic status for ${applicationId} was expected to be in ${allowedStatuses}, but it is `${currentStatus}`")
     }
 }
