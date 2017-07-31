e5c7c41ae99614dc65dabb16cd2c329f3b497e67
Giorgio Sironi <g.sironi@elifesciences.org>
2017-07-31 14:03:57 +0100

    crm--ci should be turned off when not used

--- vars/elifeCiStacks.groovy
+++ vars/elifeCiStacks.groovy
@@ -1,3 +1,3 @@
 def call() {
-    return ['api-dummy--ci', 'elife-bot--ci', 'elife-dashboard--ci', 'elife-metrics--ci', 'journal--ci', 'journal-cms--ci', 'iiif--ci', 'lax--ci', 'medium--ci', 'observer--ci', 'pattern-library--ci', 'personalised-covers--ci', 'recommendations--ci', 'redirects--ci', 'reviewer-suggestions--ci', 'search--ci']
+    return ['api-dummy--ci', 'crm--ci', 'elife-bot--ci', 'elife-dashboard--ci', 'elife-metrics--ci', 'journal--ci', 'journal-cms--ci', 'iiif--ci', 'lax--ci', 'medium--ci', 'observer--ci', 'pattern-library--ci', 'personalised-covers--ci', 'recommendations--ci', 'redirects--ci', 'reviewer-suggestions--ci', 'search--ci']
 }
