2407678cfcf5235dc55fca870c88988afde07b42
Giorgio Sironi <g.sironi@elifesciences.org>
2017-08-07 17:41:45 +0100

    New argument for elifeGithubPullRequestComment

--- vars/elifeGithubPullRequestComment.groovy
+++ vars/elifeGithubPullRequestComment.groovy
@@ -1,4 +1,12 @@
-def call(Integer prNumber, String body) {
+def call(Integer prNumber, String body, String oncePerPullRequest = null) {
     def escapedBody = escapeString(body)
+    if (oncePerPullRequest) {
+        def matchingLines = sh(script: "number=${prNumber} body='${oncePerPullRequest}' bash -x /usr/local/jenkins-scripts/check_github_pull_request_comment.sh", returnStdout: true)
+        if (matchingLines > 0) {
+            return false
+        }
+    }
+
     sh "number=${prNumber} body='${escapedBody}' bash -x /usr/local/jenkins-scripts/post_github_pull_request_comment.sh"
+    return true
 }
