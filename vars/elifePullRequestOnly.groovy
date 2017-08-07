d5ce4f63dbac4004330826e63020d01e12dce67b
Giorgio Sironi <g.sironi@elifesciences.org>
2017-08-07 14:55:34 +0100

    prNumber calculated and passed

--- vars/elifePullRequestOnly.groovy
+++ vars/elifePullRequestOnly.groovy
@@ -1,16 +1,13 @@
 def call(Closure body) {
     // env.BRANCH_NAME is only specified for multibranch projects
     // and by extension, organization folders
+    // "PR-738"
     if (env.BRANCH_NAME != null) {
-        echo "maximumNumberOfParameters: ${body.maximumNumberOfParameters}"
-        if (body.maximumNumberOfParameters == 0) {
-            body()
-        } else {
-            // env.BUILD_TAG
-            // jenkins-pull-requests-projects-pattern-library-PR-737-1
-            def prNumber = 42;
-            body(prNumber);
-        }
+        def pieces = env.BRANCH_NAME.split("-")
+        assert pieces.length == 2 : "${pieces} was expected to have 2 elements, the original string conforming to the `PR-...` template"
+        assert pieces[0] == 'PR' : "First element of ${pieces} should be `PR`"
+        int prNumber = pieces[1] as Integer
+        body(prNumber)
     } else {
         echo "Skipped actions because not on a pull request"
     }
