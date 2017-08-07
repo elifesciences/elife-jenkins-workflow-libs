cedff1edc9190496f2d5143ec7d9b6df5fe5ac01
Giorgio Sironi <g.sironi@elifesciences.org>
2017-08-07 14:42:25 +0100

    Passing (fake) PR number

--- vars/elifePullRequestOnly.groovy
+++ vars/elifePullRequestOnly.groovy
@@ -2,7 +2,14 @@ def call(Closure body) {
     // env.BRANCH_NAME is only specified for multibranch projects
     // and by extension, organization folders
     if (env.BRANCH_NAME != null) {
-        body()
+        if (body.maximumNumberOfParameters == 0) {
+            body()
+        } else {
+            // env.BUILD_TAG
+            // jenkins-pull-requests-projects-pattern-library-PR-737-1
+            def prNumber = 42;
+            body(prNumber);
+        }
     } else {
         echo "Skipped actions because not on a pull request"
     }
