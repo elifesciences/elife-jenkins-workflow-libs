8cff67474870ff978290a0319fc857de4550361b
Giorgio Sironi <g.sironi@elifesciences.org>
2017-08-08 15:44:00 +0100

    Allowing targetUrl in elifeGithubCommitStatus step

--- vars/elifeGithubCommitStatus.groovy
+++ vars/elifeGithubCommitStatus.groovy
@@ -1,3 +1,3 @@
-def call(commit, status, context, description='elifeGithubCommitStatus step') {
-    sh "commit=${commit} status=${status} context=${context} description='${description}' /usr/local/jenkins-scripts/notify_github_commit_status.sh"
+def call(commit, status, context, description='elifeGithubCommitStatus step', targetUrl='') {
+    sh "commit=${commit} status=${status} context=${context} description='${description}' targetUrl='${targetUrl}' /usr/local/jenkins-scripts/notify_github_commit_status.sh"
 }
