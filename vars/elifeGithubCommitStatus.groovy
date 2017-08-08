dd1c67b96677eb26503cca05b873b864a319ea61
Giorgio Sironi <g.sironi@elifesciences.org>
2017-08-08 15:44:48 +0100

    Convention of snake case on env variables

--- vars/elifeGithubCommitStatus.groovy
+++ vars/elifeGithubCommitStatus.groovy
@@ -1,3 +1,3 @@
 def call(commit, status, context, description='elifeGithubCommitStatus step', targetUrl='') {
-    sh "commit=${commit} status=${status} context=${context} description='${description}' targetUrl='${targetUrl}' /usr/local/jenkins-scripts/notify_github_commit_status.sh"
+    sh "commit=${commit} status=${status} context=${context} description='${description}' target_url='${targetUrl}' /usr/local/jenkins-scripts/notify_github_commit_status.sh"
 }
