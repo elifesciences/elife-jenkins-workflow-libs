ce476f02363b9f809077e1fdd083b17a2f5c1b9e
Giorgio Sironi <g.sironi@elifesciences.org>
2017-08-18 11:34:09 +0100

    Storing here elifeXpub() pipeline

---
+++ vars/elifeXpub.groovy
@@ -0,0 +1,11 @@
+elifeLibrary {
+    def commit
+    stage 'Checkout', {
+        checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://gitlab.coko.foundation/yld/xpub']]])    
+        commit = elifeGitRevision()
+    }
+    
+    stage 'Trigger dependency update', {
+         build job: 'dependencies-elife-xpub-update-xpub', wait: false, parameters: [string(name: 'commit', value: commit)]
+    }
+}
