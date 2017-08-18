900e71909a00c2d5e0fa6b956da9cff5812e9c29
Giorgio Sironi <g.sironi@elifesciences.org>
2017-08-18 14:53:12 +0100

    Missing call() definition in elifeXpub()

--- vars/elifeXpub.groovy
+++ vars/elifeXpub.groovy
@@ -1,11 +1,14 @@
-elifeLibrary {
-    def commit
-    stage 'Checkout', {
-        checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://gitlab.coko.foundation/yld/xpub']]])    
-        commit = elifeGitRevision()
-    }
-    
-    stage 'Trigger dependency update', {
-         build job: 'dependencies-elife-xpub-update-xpub', wait: false, parameters: [string(name: 'commit', value: commit)]
+def call()
+{
+    elifeLibrary {
+        def commit
+        stage 'Checkout', {
+            checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://gitlab.coko.foundation/yld/xpub']]])    
+            commit = elifeGitRevision()
+        }
+        
+        stage 'Trigger dependency update', {
+             build job: 'dependencies-elife-xpub-update-xpub', wait: false, parameters: [string(name: 'commit', value: commit)]
+        }
     }
 }
