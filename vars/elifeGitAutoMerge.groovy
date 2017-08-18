06e013141ade195ac58f69821c1f5e5f9879f352
Giorgio Sironi <g.sironi@elifesciences.org>
2017-08-18 10:13:56 +0100

    Quoting, but not escaping, title

--- vars/elifeGitAutoMerge.groovy
+++ vars/elifeGitAutoMerge.groovy
@@ -4,6 +4,7 @@ def call(branch, title, description = '', base='master') {
     sh "git pull origin ${localBase}"
     // TODO: also use description, but it's complex to escape it to pass in on the command line
     // maybe if there's a way to specify a file like in elifeGithubPullRequest
-    sh "git merge ${branch} -m ${title}"
+    // TODO: escape `title`
+    sh "git merge ${branch} -m '${title}'"
     sh "git push origin ${localBase}"
 }
