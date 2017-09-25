d918429704cdaf42c9476cfc8c7115016fc49266
Giorgio Sironi <g.sironi@elifesciences.org>
2017-09-25 13:45:59 +0100

    Added elifeFormula pipeline template

---
+++ vars/elifeFormula.groovy
@@ -0,0 +1,33 @@
+def elifeFormula(String project, String formula = null) {
+    if (formula == null) {
+        formula = "${project}-formula"
+    }
+    elifePipeline {
+        def commit
+        stage 'Checkout', {
+            checkout scm
+            commit = elifeGitRevision()
+        }
+
+        elifePullRequestOnly { prNumber ->
+            def instance = "pr-${prNumber}"
+            def stackname = "${project}--${instance}"
+            try {
+                stage 'Basic stack', {
+                    sh "/srv/builder/bldr ensure_destroyed:${stackname}"
+                    sh "/srv/builder/bldr masterless.launch:${project},${instance}"
+                }
+
+                stage 'Applying change', {
+                    sh "/srv/builder/bldr masterless.set_versions:${stackname},${formula}@${commit}"
+                    sh "/srv/builder/bldr update:${stackname}"
+                    // TODO: run smoke tests
+                }
+            } finally {
+                stage 'Cleanup', {
+                    sh "/srv/builder/bldr ensure_destroyed:${stackname}"
+                }
+            }
+        }
+    }
+}
