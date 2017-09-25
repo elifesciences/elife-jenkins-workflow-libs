dea39c786defbe0ee55d99bf81a176874ad4a65c
Giorgio Sironi <g.sironi@elifesciences.org>
2017-09-25 14:34:28 +0100

    Allowing smoke tests specification in formula pipeline

--- vars/elifeFormula.groovy
+++ vars/elifeFormula.groovy
@@ -1,4 +1,4 @@
-def call(String project, String formula = null) {
+def call(String project, String smokeTestsFolder = '', String formula = null) {
     if (formula == null) {
         formula = "${project}-formula"
     }
@@ -21,7 +21,12 @@ def call(String project, String formula = null) {
                 stage 'Applying change', {
                     sh "/srv/builder/bldr masterless.set_versions:${stackname},${formula}@${commit}"
                     sh "/srv/builder/bldr update:${stackname}"
-                    // TODO: run smoke tests
+                }
+
+                if (smokeTestsFolder) {
+                    stage 'Smoke tests', {
+                        builderSmokeTests stackname, smokeTestsFolder
+                    }
                 }
             } finally {
                 stage 'Cleanup', {
