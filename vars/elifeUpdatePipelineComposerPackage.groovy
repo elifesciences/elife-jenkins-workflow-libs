84e925108232e1d16cfb39a0884c34da037f6680
Giorgio Sironi <g.sironi@elifesciences.org>
2017-10-02 15:06:47 +0100

    Updated elifeUpdatePipelineComposerPackage to only commit updates of the main package, not of dependencies

--- vars/elifeUpdatePipelineComposerPackage.groovy
+++ vars/elifeUpdatePipelineComposerPackage.groovy
@@ -12,14 +12,23 @@ def call(Map parameters) {
     def String basePackageName = packageName.split("/")[1]
     def String branchPrefix = basePackageName.replaceAll("[^a-z0-9]", "_")
     def String additionalPackageNames = parameters.get('additionalPackageNames', []).join(" ")
+    def String currentVersion = {
+        def version = sh(script: "jq '.packages[] | select(.name==\"${packageName}\").dist.reference' < composer.lock", returnStdout: true)
+        echo "Version of {$packageName}: ${version}"
+        return version
+    }
 
     elifeUpdatePipeline(
         { commit ->
             lock(stackname) {
+                def String originalVersion = currentVersion()
                 builderDeployRevision stackname, commit
                 builderCmd stackname, "composer update ${additionalPackageNames} ${packageName} --with-dependencies --no-suggest --no-interaction", folder
                 builderSync hostname, folder
-                sh "git add composer.lock"
+                def String newVersion = currentVersion()
+                if (newVersion != originalVersion) {
+                    sh "git add composer.lock"
+                }
             }
         },
         {
