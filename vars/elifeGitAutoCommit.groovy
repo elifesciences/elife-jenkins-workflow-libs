cdbea0ecfabab53ae83eae4c450b290633dcbb12
Giorgio Sironi <g.sironi@elifesciences.org>
2017-10-17 15:01:59 +0100

    Supporting multiple values in elifeGitAutoCommit step

--- vars/elifeGitAutoCommit.groovy
+++ vars/elifeGitAutoCommit.groovy
@@ -1,5 +1,10 @@
-def call(message = "Automatic commit", add=".") {
-    sh "git add --all --force ${add}"
+def call(message = "Automatic commit", String... add) {
+    if (add.length > 0) {
+        path = add.join(" ")
+    } else {
+        path = "."
+    }
+    sh "git add --all --force ${path}"
     def code = sh script: "git diff --quiet --exit-code --cached", returnStatus: true
     echo "git diff exit code: ${code}"
     if (code) {
