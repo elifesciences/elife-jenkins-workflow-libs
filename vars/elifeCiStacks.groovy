c049fa12f91ccc19b810f49f4554b7fbe2ed4e41
Giorgio Sironi <g.sironi@elifesciences.org>
2017-10-25 14:55:41 +0100

    Added annotations automated testing stacks

--- vars/elifeCiStacks.groovy
+++ vars/elifeCiStacks.groovy
@@ -1,3 +1,3 @@
 def call() {
-    return ['api-dummy--ci', 'crm--ci', 'elife-bot--ci', 'elife-dashboard--ci', 'elife-ink--ci', 'elife-metrics--ci', 'elife-xpub--ci', 'journal--ci', 'journal-cms--ci', 'iiif--ci', 'lax--ci', 'medium--ci', 'observer--ci', 'pattern-library--ci', 'peerscout--ci', 'personalised-covers--ci', 'profiles--ci', 'recommendations--ci', 'redirects--ci', 'schematron-validator--ci', 'search--ci']
+    return ['api-dummy--ci', 'annotations--ci', 'crm--ci', 'elife-bot--ci', 'elife-dashboard--ci', 'elife-ink--ci', 'elife-metrics--ci', 'elife-xpub--ci', 'journal--ci', 'journal-cms--ci', 'iiif--ci', 'lax--ci', 'medium--ci', 'observer--ci', 'pattern-library--ci', 'peerscout--ci', 'personalised-covers--ci', 'profiles--ci', 'recommendations--ci', 'redirects--ci', 'schematron-validator--ci', 'search--ci']
 }
