a61e50532ac02cc358c76c296292ffb05f9e98bf
Giorgio Sironi <g.sironi@elifesciences.org>
2017-10-30 15:59:29 +0000

    Adding a third powerful instance for tests

--- vars/elifeSlaveStacks.groovy
+++ vars/elifeSlaveStacks.groovy
@@ -1,3 +1,3 @@
 def call() {
-    return ['elife-libraries--ci', 'elife-libraries--powerful', 'elife-libraries--powerful2']
+    return ['elife-libraries--ci', 'elife-libraries--powerful', 'elife-libraries--powerful2', 'elife-libraries--powerful3']
 }
