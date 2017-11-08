b5dddf7515dcc7426889b728521444cfea8898fc
Giorgio Sironi <g.sironi@elifesciences.org>
2017-11-08 10:06:52 +0000

    Add changes link to Slack notification

--- vars/elifeDeploySlackNotification.groovy
+++ vars/elifeDeploySlackNotification.groovy
@@ -1,3 +1,3 @@
 def call(project, environment) {
-    elifeSlack "Deploying *${project}* on *${environment}* (<${env.BUILD_URL}|Build>)"
+    elifeSlack "Deploying *${project}* on *${environment}* (<${env.RUN_DISPLAY_URL}|Build>, <${env.RUN_CHANGES_DISPLAY_URL}|Changes>)"
 }
