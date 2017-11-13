2454d2bbb60d24dcb84bc1c766c38dd7c8f8ff63
Giorgio Sironi <g.sironi@elifesciences.org>
2017-11-13 10:56:35 +0000

    Adding lock on stacknames during formula builds

--- vars/elifeFormula.groovy
+++ vars/elifeFormula.groovy
@@ -12,39 +12,41 @@ def call(String project, String smokeTestsFolder = '', String formula = null) {
         elifePullRequestOnly { prNumber ->
             def instance = "${formula}-pr-${prNumber}"
             def stackname = "${project}--${instance}"
-            try {
-                stage 'Basic stack', {
-                    try {
-                        elifeGithubCommitStatus commit, 'pending', 'continuous-integration/jenkins/pr-base', 'Original stack creation started', env.RUN_DISPLAY_URL
-                        sh "/srv/builder/bldr ensure_destroyed:${stackname}"
-                        sh "/srv/builder/bldr masterless.launch:${project},${instance}"
-                        if (smokeTestsFolder) {
-                            builderSmokeTests stackname, smokeTestsFolder
+            lock (stackname) {
+                try {
+                    stage 'Basic stack', {
+                        try {
+                            elifeGithubCommitStatus commit, 'pending', 'continuous-integration/jenkins/pr-base', 'Original stack creation started', env.RUN_DISPLAY_URL
+                            sh "/srv/builder/bldr ensure_destroyed:${stackname}"
+                            sh "/srv/builder/bldr masterless.launch:${project},${instance}"
+                            if (smokeTestsFolder) {
+                                builderSmokeTests stackname, smokeTestsFolder
+                            }
+                            elifeGithubCommitStatus commit, 'success', 'continuous-integration/jenkins/pr-base', 'Original stack creation succeeded', env.RUN_DISPLAY_URL
+                        } catch (e) {
+                            elifeGithubCommitStatus commit, 'failure', 'continuous-integration/jenkins/pr-base', 'Original stack creation failed', env.RUN_DISPLAY_URL
+                            throw e
                         }
-                        elifeGithubCommitStatus commit, 'success', 'continuous-integration/jenkins/pr-base', 'Original stack creation succeeded', env.RUN_DISPLAY_URL
-                    } catch (e) {
-                        elifeGithubCommitStatus commit, 'failure', 'continuous-integration/jenkins/pr-base', 'Original stack creation failed', env.RUN_DISPLAY_URL
-                        throw e
                     }
-                }
 
-                stage 'Applying change', {
-                    try {
-                        elifeGithubCommitStatus commit, 'pending', 'continuous-integration/jenkins/pr-update', 'Applying update started', env.RUN_DISPLAY_URL
-                        sh "/srv/builder/bldr masterless.set_versions:${stackname},${formula}@${commit}"
-                        sh "/srv/builder/bldr update:${stackname}"
-                        if (smokeTestsFolder) {
-                            builderSmokeTests stackname, smokeTestsFolder
+                    stage 'Applying change', {
+                        try {
+                            elifeGithubCommitStatus commit, 'pending', 'continuous-integration/jenkins/pr-update', 'Applying update started', env.RUN_DISPLAY_URL
+                            sh "/srv/builder/bldr masterless.set_versions:${stackname},${formula}@${commit}"
+                            sh "/srv/builder/bldr update:${stackname}"
+                            if (smokeTestsFolder) {
+                                builderSmokeTests stackname, smokeTestsFolder
+                            }
+                            elifeGithubCommitStatus commit, 'success', 'continuous-integration/jenkins/pr-update', 'Applying update succeeded', env.RUN_DISPLAY_URL
+                        } catch (e) {
+                            elifeGithubCommitStatus commit, 'failure', 'continuous-integration/jenkins/pr-update', 'Applying update failed', env.RUN_DISPLAY_URL
+                            throw e
                         }
-                        elifeGithubCommitStatus commit, 'success', 'continuous-integration/jenkins/pr-update', 'Applying update succeeded', env.RUN_DISPLAY_URL
-                    } catch (e) {
-                        elifeGithubCommitStatus commit, 'failure', 'continuous-integration/jenkins/pr-update', 'Applying update failed', env.RUN_DISPLAY_URL
-                        throw e
                     }
-                }
-            } finally {
-                stage 'Cleanup', {
-                    sh "/srv/builder/bldr ensure_destroyed:${stackname}"
+                } finally {
+                    stage 'Cleanup', {
+                        sh "/srv/builder/bldr ensure_destroyed:${stackname}"
+                    }
                 }
             }
         }
