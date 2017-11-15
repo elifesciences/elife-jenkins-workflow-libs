fd002736ad52c288cff0726816ce9ac0838a236d
Giorgio Sironi <g.sironi@elifesciences.org>
2017-11-15 14:55:22 +0000

    More generic name for variant stages

--- vars/elifeVariants.groovy
+++ vars/elifeVariants.groovy
@@ -1,4 +1,4 @@
-def call(variants, build, stageName='Tests, %s dependencies') {
+def call(variants, build, stageName='Tests, %s variant') {
     for (int i = 0; i < variants.size(); i++) {
         def variant = variants.get(i)
 
