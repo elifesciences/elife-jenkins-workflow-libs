54f588e3304e82999ed9f9dc8c7a18ec88fc90c9
Giorgio Sironi <g.sironi@elifesciences.org>
2017-12-01 14:02:52 +0000

    Support List cmd arguments in builderRunAll() steps

--- vars/builderRunAll.groovy
+++ vars/builderRunAll.groovy
@@ -1,11 +1,16 @@
 def call(stacks, cmd, concurrency='serial') {
+    if (!(cmd instanceof List)) {
+        cmd = [cmd]
+    }
     def actions = [:]
     for (int i = 0; i < stacks.size(); i++) {
         def stack = stacks.get(i)
         actions[stack] = {
             lock(stack) {
                 builderStart(stack)
-                builderCmd(stack, cmd, null, false, concurrency)
+                for (i = 0; i < cmd.size(); i++) { 
+                    builderCmd(stack, cmd.get(i), null, false, concurrency)
+                }
             }
         }
     }
