3304ec25f50d3061c13c7261757da5ac1dd2ef9e
Giorgio Sironi <g.sironi@elifesciences.org>
2017-12-05 09:13:55 +0000

    Typo in stack variable name

--- vars/builderRunAll.groovy
+++ vars/builderRunAll.groovy
@@ -9,7 +9,7 @@ def call(stacks, cmd, branch=null, stackConcurrency='serial') {
             lock(stack) {
                 builderStart(stack)
                 if (branch) {
-                    sh "${env.BUILDER_PATH}bldr 'buildvars.switch_revision:${stackname},${branch},concurrency=${stackConcurrency}'"
+                    sh "${env.BUILDER_PATH}bldr 'buildvars.switch_revision:${stack},${branch},concurrency=${stackConcurrency}'"
                 }
                 for (i = 0; i < cmd.size(); i++) { 
                     builderCmd(stack, cmd.get(i), null, false, stackConcurrency)
