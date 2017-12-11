37915689279b0387335e05c56944291814cbe826
Giorgio Sironi <g.sironi@elifesciences.org>
2017-12-11 15:28:40 +0000

    Add continuumtest stacks list

---
+++ vars/elifeContinuumtestStacks.groovy
@@ -0,0 +1,21 @@
+def call() {
+    return [
+        'annotations--continuumtest',
+        'api-gateway--continuumtest',
+        'elife-bot--continuumtest',
+        'elife-dashboard--continuumtest',
+        'elife-metrics--continuumtest',
+        'elife-website--continuumtest',
+        'iiif--continuumtest',
+        'journal--continuumtest',
+        'journal-cms--continuumtest',
+        'lax--continuumtest',
+        'medium--continuumtest',
+        //'observer--continuumtest',
+        //'peerscout--continuumtest',
+        'personalised-covers--continuumtest',
+        'profiles--continuumtest',
+        'recommendations--continuumtest',
+        'search--continuumtest'
+    ]
+}
