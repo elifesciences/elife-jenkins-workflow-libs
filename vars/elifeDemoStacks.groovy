eaf8a390a1849e3f0b66fbfc14d04a5959aa6fd4
Giorgio Sironi <g.sironi@elifesciences.org>
2018-01-11 10:06:17 +0000

    Add elifeDemoStacks() and elifeProdStacks() list

---
+++ vars/elifeDemoStacks.groovy
@@ -0,0 +1,8 @@
+def call() {
+    return [
+        'api-dummy--demo',
+        'elife-ink--demo',
+        'elife-xpub--demo',
+        'journal--demo'
+    ]
+}
---
+++ vars/elifeProdStacks.groovy
@@ -0,0 +1,27 @@
+def call() {
+    return [
+        'annotations--prod',
+        'api-gateway--prod',
+        'crm--prod',
+        'elife-api--legacy',
+        'jats4r--2016-08-22',
+        'elife-bot--prod',
+        'elife-dashboard--prod',
+        'elife-metrics--prod',
+        'iiif--prod',
+        'journal--preview',
+        'journal--prod',
+        'journal-cms--prod',
+        'lax--prod',
+        'medium--prod',
+        'observer--prod',
+        'pattern-library--prod',
+        'peerscout--prod',
+        'personalised-covers--prod',
+        'profiles--prod',
+        'recommendations--prod',
+        'redirects--prod',
+        'schematron-validator--prod',
+        'search--prod'
+    ]
+}
