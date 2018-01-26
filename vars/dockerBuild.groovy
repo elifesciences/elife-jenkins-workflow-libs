183194b0f6a59c17dc39e1452b9fa162c04ffb71
Giorgio Sironi <g.sironi@elifesciences.org>
2018-01-26 16:24:16 +0000

    The world isn't ready for the creation of objects from steps

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,5 @@
 def call(project, tag='latest', organization='elifesciences') {
     def imageName = "${organization}/${project}"
     sh "docker build --pull -t ${imageName}:${tag} ."
-    return new DockerImage(this)
+    //return new DockerImage(this)
 }
