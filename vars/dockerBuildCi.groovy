1a4f60185d4fb00b94f4a94215b811f9f322dcb0
Giorgio Sironi <g.sironi@elifesciences.org>
2018-02-08 16:16:31 +0000

    Add missing suffix to `ci` image name

--- vars/dockerBuildCi.groovy
+++ vars/dockerBuildCi.groovy
@@ -1,4 +1,4 @@
 def call(project, commit, organization='elifesciences') {
     def imageName = "${organization}/${project}"
-    sh "docker build -f Dockerfile.ci -t ${imageName}:${commit} --build-arg commit=${commit} ."
+    sh "docker build -f Dockerfile.ci -t ${imageName}_ci:${commit} --build-arg commit=${commit} ."
 }
