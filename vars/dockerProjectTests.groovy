276da25295dce3d2339082e615e62dbd672ee0a1
Giorgio Sironi <g.sironi@elifesciences.org>
2018-02-14 15:02:58 +0000

    Explain PROJECT_FOLDER requirement

--- vars/dockerProjectTests.groovy
+++ vars/dockerProjectTests.groovy
@@ -1,6 +1,9 @@
 def call(project, commit, /*testArtifacts=[],*/ organization='elifesciences') {
     def fullImageName = "${organization}/${project}_ci:${commit}"
     def folder = dockerReadEnv fullImageName, 'PROJECT_FOLDER'
+    if (!folder) {
+        throw new Exception("The PROJECT_FOLDER environment variable must be configured in the container image, for the projects tests to be correctly located.")
+    }
     sh "chmod 777 build/ && docker run -v \$(pwd)/build:${folder}/build ${fullImageName}"
     step([$class: 'JUnitResultArchiver', testResults: 'build/*.xml'])
 }
