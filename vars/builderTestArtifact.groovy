dc9074e35aba22356a14aaa133a2d34cc96f8241
Giorgio Sironi <g.sironi@elifesciences.org>
2018-02-23 10:23:21 +0000

    Do not allow missing files with wildcars, to trigger multiple downloads

--- vars/builderTestArtifact.groovy
+++ vars/builderTestArtifact.groovy
@@ -5,10 +5,14 @@ def call(remoteTestArtifact, stackname) {
     def localTestArtifact = ((remoteTestArtifact =~ /\/(build\/.*)/)[0][1])
     def slash = localTestArtifact.lastIndexOf('/')
     def localTestArtifactFolder = localTestArtifact[0..slash]
+    def allowMissing = ",allow_missing=True"
+    if (localTestArtifact.contains('*')) {
+        allowMissing = ""
+    }
 
     // builder runs in its own folder as working directory
     echo "Downloading on ${localTestArtifact}"
-    sh "${env.BUILDER_PATH}bldr download_file:${stackname},${remoteTestArtifact},${env.WORKSPACE}/${localTestArtifactFolder},allow_missing=True"
+    sh "${env.BUILDER_PATH}bldr download_file:${stackname},${remoteTestArtifact},${env.WORKSPACE}/${localTestArtifactFolder}{$allowMissing}"
 
     if (!readFile(localTestArtifact)) {
         echo "Artifact ${localTestArtifact} not found"
