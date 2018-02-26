aae0d8118e4e0b01d830c45878761b21ba5fb94d
Giorgio Sironi <g.sironi@elifesciences.org>
2018-02-26 11:35:01 +0000

    Return Closure from withCommitStatus

--- vars/withCommitStatus.groovy
+++ vars/withCommitStatus.groovy
@@ -1,10 +1,12 @@
 def call(Closure body, String name, String commit) {
-    elifeGithubCommitStatus commit, 'pending', "continuous-integration/jenkins/${name}", "${name} started", env.RUN_DISPLAY_URL
-    try {
-        body()
-        elifeGithubCommitStatus commit, 'success', "continuous-integration/jenkins/${name}", "${name} succeeded", env.RUN_DISPLAY_URL
-    } catch (e) {
-        elifeGithubCommitStatus commit, 'failure', "continuous-integration/jenkins/${name}", "${name} failed: ${e.message}", env.RUN_DISPLAY_URL
-        throw e
+    return {
+        elifeGithubCommitStatus commit, 'pending', "continuous-integration/jenkins/${name}", "${name} started", env.RUN_DISPLAY_URL
+        try {
+            body()
+            elifeGithubCommitStatus commit, 'success', "continuous-integration/jenkins/${name}", "${name} succeeded", env.RUN_DISPLAY_URL
+        } catch (e) {
+            elifeGithubCommitStatus commit, 'failure', "continuous-integration/jenkins/${name}", "${name} failed: ${e.message}", env.RUN_DISPLAY_URL
+            throw e
+        }
     }
 }
