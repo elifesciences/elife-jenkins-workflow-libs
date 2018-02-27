384f9cb983314f21e782c98e5638ed02bb14d098
Giorgio Sironi <g.sironi@elifesciences.org>
2018-02-27 14:58:53 +0000

    Add regression tests for path()

--- test/groovy/TestRemoteTestArtifact.groovy
+++ test/groovy/TestRemoteTestArtifact.groovy
@@ -7,6 +7,7 @@ class TestRemoteTestArtifact {
     @Test
     void calculates_local_paths_from_relative_path() throws Exception {
         def a = new RemoteTestArtifact('build/phpunit.xml');
+        assertEquals('build/phpunit.xml', a.path())
         assertEquals('build/', a.localTestArtifactFolder())
         assertEquals('build/phpunit.xml', a.localTestArtifact())
     }
@@ -14,6 +15,7 @@ class TestRemoteTestArtifact {
     @Test
     void calculates_local_paths_from_absolute_path() throws Exception {
         def a = new RemoteTestArtifact('/srv/journal/build/phpunit.xml');
+        assertEquals('/srv/journal/build/phpunit.xml', a.path())
         assertEquals('build/', a.localTestArtifactFolder())
         assertEquals('build/phpunit.xml', a.localTestArtifact())
     }
@@ -21,6 +23,7 @@ class TestRemoteTestArtifact {
     @Test
     void works_with_star_matchers() throws Exception {
         def a = new RemoteTestArtifact('/srv/journal/build/phpunit/*.xml');
+        assertEquals('/srv/journal/build/phpunit/*.xml', a.path())
         assertEquals('build/phpunit/', a.localTestArtifactFolder())
         assertEquals('build/phpunit/*.xml', a.localTestArtifact())
         assertEquals('/srv/journal/build/phpunit/', a.remoteTestArtifactFolder())
