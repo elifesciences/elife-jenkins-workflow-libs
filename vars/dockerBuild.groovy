242034c39ecbb0ffbbaf0e25ff99c20ffef129fd
Giorgio Sironi <g.sironi@elifesciences.org>
2018-02-27 15:26:19 +0000

    Add dockerfileSuffix optional parameter to dockerBuild

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,9 @@
-def call(project, tag='latest', organization='elifesciences') {
+def call(project, tag='latest', dockerfileSuffix = null, organization='elifesciences') {
     def imageName = "${organization}/${project}"
-    sh "docker build --pull -t ${imageName}:${tag} ."
+    def dockerfile = 'Dockerfile'
+    if (dockerfileSuffix) {
+        dockerfile = "${dockerfile}.${dockerfileSuffix}"
+    }
+    sh "docker build --pull -f ${dockerfile} -t ${imageName}:${tag} ."
     //return new DockerImage(this)
 }
