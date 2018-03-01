d4842fcff87b408a2635514f26d83427b6caa9a9
Giorgio Sironi <g.sironi@elifesciences.org>
2018-03-01 14:11:18 +0000

    Wrap in finally retrieval of artifacts

--- vars/dockerProjectTests.groovy
+++ vars/dockerProjectTests.groovy
@@ -4,8 +4,11 @@ def call(project, commit, testArtifacts=['build/*.xml'], organization='elifescie
     if (!folder) {
         throw new Exception("The PROJECT_FOLDER environment variable must be configured in the container image, for the projects tests to be correctly located.")
     }
-    sh "chmod 777 build/ && docker run -v \$(pwd)/build:${folder}/build ${fullImageName}"
-    for (int i = 0; i < testArtifacts.size(); i++) {
-        step([$class: 'JUnitResultArchiver', testResults: testArtifacts.get(i)])
+    try {
+        sh "chmod 777 build/ && docker run -v \$(pwd)/build:${folder}/build ${fullImageName}"
+    } finally { 
+        for (int i = 0; i < testArtifacts.size(); i++) {
+            step([$class: 'JUnitResultArchiver', testResults: testArtifacts.get(i)])
+        }
     }
 }
