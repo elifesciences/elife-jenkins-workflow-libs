8fde4025f004e47d586c7e44e86cd79e64d43ce7
Giorgio Sironi <g.sironi@elifesciences.org>
2018-03-01 14:04:35 +0000

    Allow customization of test artifacts

--- vars/dockerProjectTests.groovy
+++ vars/dockerProjectTests.groovy
@@ -1,9 +1,11 @@
-def call(project, commit, /*testArtifacts=[],*/ organization='elifesciences') {
+def call(project, commit, testArtifacts=['build/*.xml'], organization='elifesciences') {
     def fullImageName = "${organization}/${project}_ci:${commit}"
     def folder = dockerReadEnv fullImageName, 'PROJECT_FOLDER'
     if (!folder) {
         throw new Exception("The PROJECT_FOLDER environment variable must be configured in the container image, for the projects tests to be correctly located.")
     }
     sh "chmod 777 build/ && docker run -v \$(pwd)/build:${folder}/build ${fullImageName}"
-    step([$class: 'JUnitResultArchiver', testResults: 'build/*.xml'])
+    for (int i = 0; i < testArtifacts.size(); i++) {
+        step([$class: 'JUnitResultArchiver', testResults: testArtifacts.get(i)])
+    }
 }
