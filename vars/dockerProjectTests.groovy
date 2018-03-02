55c97a09626a86b02049bb6670e423c583fbadcb
Giorgio Sironi <g.sironi@elifesciences.org>
2018-03-02 16:44:20 +0000

    Restructure test artifacts retrieval of dockerProjectTests

--- vars/dockerProjectTests.groovy
+++ vars/dockerProjectTests.groovy
@@ -1,13 +1,17 @@
-def call(project, commit, testArtifacts=['build/*.xml'], organization='elifesciences') {
+def call(project, commit, testArtifacts=[], organization='elifesciences') {
     def fullImageName = "${organization}/${project}_ci:${commit}"
+    def String container = "${project}_ci_project_tests"
     def folder = dockerReadEnv fullImageName, 'PROJECT_FOLDER'
     if (!folder) {
         throw new Exception("The PROJECT_FOLDER environment variable must be configured in the container image, for the projects tests to be correctly located.")
     }
     try {
-        sh "chmod 777 build/ && docker run -v \$(pwd)/build:${folder}/build ${fullImageName}"
+        sh "docker rm ${container} || true"
+        sh "docker run --name ${container} ${fullImageName}"
     } finally { 
         for (int i = 0; i < testArtifacts.size(); i++) {
+            def remoteTestArtifact = new RemoteTestArtifact(testArtifacts.get(i))
+            sh "docker cp ${container}:${remoteTestArtifact.remoteTestArtifactFolder()}/. ${remoteTestArtifact.localTestArtifactFolder()}"
             step([$class: 'JUnitResultArchiver', testResults: testArtifacts.get(i)])
         }
     }
