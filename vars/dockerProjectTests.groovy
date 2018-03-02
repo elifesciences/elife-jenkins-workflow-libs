42ff39d0bf63926535b7ad24e4e35b40e251bc1f
Giorgio Sironi <g.sironi@elifesciences.org>
2018-03-02 16:54:26 +0000

    Automatically retrieve test artifacts in dockerProjectTests

--- vars/dockerProjectTests.groovy
+++ vars/dockerProjectTests.groovy
@@ -1,9 +1,12 @@
 def call(project, commit, testArtifacts=[], organization='elifesciences') {
     def fullImageName = "${organization}/${project}_ci:${commit}"
     def String container = "${project}_ci_project_tests"
-    def folder = dockerReadEnv fullImageName, 'PROJECT_FOLDER'
-    if (!folder) {
-        throw new Exception("The PROJECT_FOLDER environment variable must be configured in the container image, for the projects tests to be correctly located.")
+    if (!testArtifacts) {
+        def folder = dockerReadEnv fullImageName, 'PROJECT_FOLDER'
+        if (!folder) {
+            throw new Exception("The PROJECT_FOLDER environment variable must be configured in the container image, for the projects test artifacts to be correctly located. Otherwise, specify the `testArtifacts` argument, for example `/srv/api-dummy/build/*.xml`")
+        }
+        testArtifacts = ["${folder}/build/*.xml"]
     }
     try {
         sh "docker rm ${container} || true"
