ff9ef800a81a4468d679be7ee20e88434437c3aa
Giorgio Sironi <g.sironi@elifesciences.org>
2018-03-08 16:30:13 +0000

    Rollback use of Revision

--- vars/elifeGithubCommitStatus.groovy
+++ vars/elifeGithubCommitStatus.groovy
@@ -1,10 +1,10 @@
 import Revision
 
 def call(commit, status, context, description='elifeGithubCommitStatus step', targetUrl='') {
-    def revision = new Revision(commit)
-    if (revision.isBranch()) {
-        echo "No commit status to update for branch ${revision}"
-    } else {
-        sh "commit=${revision} status=${status} context=${context} description='${description}' target_url='${targetUrl}' /usr/local/jenkins-scripts/notify_github_commit_status.sh"
-    }
+    //def revision = new Revision(commit)
+    //if (revision.isBranch()) {
+    //    echo "No commit status to update for branch ${revision}"
+    //} else {
+        sh "commit=${commit} status=${status} context=${context} description='${description}' target_url='${targetUrl}' /usr/local/jenkins-scripts/notify_github_commit_status.sh"
+    //}
 }
