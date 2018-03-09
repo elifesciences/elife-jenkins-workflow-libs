24887d19e210eb4b4bd78d2f3f06ea560fa77fbe
Giorgio Sironi <g.sironi@elifesciences.org>
2018-03-09 13:35:22 +0000

    Use Revision again, more carefully

--- vars/elifeGithubCommitStatus.groovy
+++ vars/elifeGithubCommitStatus.groovy
@@ -1,10 +1,10 @@
 import Revision
 
 def call(commit, status, context, description='elifeGithubCommitStatus step', targetUrl='') {
-    //def revision = new Revision(commit)
-    //if (revision.isBranch()) {
-    //    echo "No commit status to update for branch ${revision}"
-    //} else {
+    def revision = new Revision(commit)
+    if (revision.isBranch()) {
+        echo "No commit status to update for branch ${revision.toString()}"
+    } else {
         sh "commit=${commit} status=${status} context=${context} description='${description}' target_url='${targetUrl}' /usr/local/jenkins-scripts/notify_github_commit_status.sh"
-    //}
+    }
 }
