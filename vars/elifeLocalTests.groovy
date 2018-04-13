640a572f276304b88085b20d7cbe96644e62bbdc
Giorgio Sironi <g.sironi@elifesciences.org>
2018-04-13 17:05:08 +0100

    Support .ci parallel tests in elifeLocalTests

--- vars/elifeLocalTests.groovy
+++ vars/elifeLocalTests.groovy
@@ -1,6 +1,34 @@
+def defineLocalTests() {
+    def String commit = elifeGitRevision()
+    def actions = [:]
+    def projectTestsParallelScripts = findFiles(glob: '.ci/*')
+    for (int i = 0; i < projectTestsParallelScripts.size(); i++) {
+        def name = "${projectTestsParallelScripts[i].name}"
+        actions[name] = {
+            withCommitStatus({
+                sh "${projectTestsParallelScripts[i].path}"
+            }, name, commit)
+        }
+    }
+    if (fileExists('project_tests.sh')) {
+        actions['project_tests.sh'] = {
+            withCommitStatus({
+                sh 'project_tests.sh'
+            }, 'project_tests', commit)
+        }
+    }
+
+    if (!actions) {
+        throw new Exception("No .ci/ or project_tests.sh script was found")
+    }
+
+    return actions
+}
+
 def call(cmd, testArtifacts=[]) {
+    actions = defineLocalTests()
     try {
-        sh cmd
+        parallel actions
     } finally {
         for (int i = 0; i < testArtifacts.size(); i++) {
             def localTestArtifact = testArtifacts.get(i)
