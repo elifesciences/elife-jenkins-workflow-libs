537adea73f3188346a8eae108b38fb3b4ccd201e
Giorgio Sironi <g.sironi@elifesciences.org>
2018-05-09 12:13:47 +0100

    _defineProjectTests produces actions with their own commit statuses

--- vars/dockerComposeProjectTestsParallel.groovy
+++ vars/dockerComposeProjectTestsParallel.groovy
@@ -3,30 +3,28 @@ import DockerCompose
 def call(project, tag='latest', testArtifacts=[:])
 {
     action = { stackname, command, folder, label ->
-        withCommitStatus({
-            def String container = "${project}_ci_${label}"
-            try {
-                sh "docker rm ${container} --volumes || true"
+        def String container = "${project}_ci_${label}"
+        try {
+            sh "docker rm ${container} --volumes || true"
 
-                sh DockerCompose
-                    .command('run', ['docker-compose.yml', 'docker-compose.ci.yml'])
-                    .withEnvironment('IMAGE_TAG', tag)
-                    .withOption('name', container)
-                    .withArgument('ci')
-                    .withArgument(command)
-                    .toString()
-            } finally {
-                if (testArtifacts.containsKey(label)) {
-                    def remoteTestArtifact = new RemoteTestArtifact(testArtifacts.get(label))
-                    sh "mkdir -p ${remoteTestArtifact.localTestArtifactFolder()}"
-                    sh "docker cp ${container}:${remoteTestArtifact.remoteTestArtifactFolder()}/. ${remoteTestArtifact.localTestArtifactFolder()}"
-                    step([$class: "JUnitResultArchiver", testResults: remoteTestArtifact.localTestArtifact()])
-                } else {
-                    echo "No test artifacts defined for ${label}"
-                }
-                sh "docker rm ${container} --volumes"
+            sh DockerCompose
+                .command('run', ['docker-compose.yml', 'docker-compose.ci.yml'])
+                .withEnvironment('IMAGE_TAG', tag)
+                .withOption('name', container)
+                .withArgument('ci')
+                .withArgument(command)
+                .toString()
+        } finally {
+            if (testArtifacts.containsKey(label)) {
+                def remoteTestArtifact = new RemoteTestArtifact(testArtifacts.get(label))
+                sh "mkdir -p ${remoteTestArtifact.localTestArtifactFolder()}"
+                sh "docker cp ${container}:${remoteTestArtifact.remoteTestArtifactFolder()}/. ${remoteTestArtifact.localTestArtifactFolder()}"
+                step([$class: "JUnitResultArchiver", testResults: remoteTestArtifact.localTestArtifact()])
+            } else {
+                echo "No test artifacts defined for ${label}"
             }
-        }, 'project-tests', tag)
+            sh "docker rm ${container} --volumes"
+        }
     }
     def actions = _defineProjectTests('unused', '/srv/journal', action)
     try {
