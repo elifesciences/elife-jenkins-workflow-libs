069693a6c6e5cb7b49d884b5241ab9d07be13780
Giorgio Sironi <g.sironi@elifesciences.org>
2018-06-06 14:53:32 +0100

    Print issuesList for debugging

--- vars/elifeGithubPullRequest.groovy
+++ vars/elifeGithubPullRequest.groovy
@@ -2,6 +2,7 @@ import EscapeString
 
 def call(branch, title, description = '', base='master', temporaryFile = 'pull-request.log') {
     def issuesList = sh script: "hub issue", returnStdout: true
+    echo "Issues list: $issuesList"
     if (issuesList.contains(title)) {
         echo "There is already an existing PR with title: ${title}"
         return
