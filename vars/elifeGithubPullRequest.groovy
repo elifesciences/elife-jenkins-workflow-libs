5ffd9c8edc09e70c5d7c63eb7bf1c758cf0941f4
Giorgio Sironi <g.sironi@elifesciences.org>
2018-06-06 12:06:44 +0100

    Do not create more than one PR with the same title

--- vars/elifeGithubPullRequest.groovy
+++ vars/elifeGithubPullRequest.groovy
@@ -1,4 +1,9 @@
 def call(branch, title, description = '', base='master', temporaryFile = 'pull-request.log') {
+    def isThereAnExistingPullRequest = (sh script: "hub issue | grep \"$title\"", returnStatus: true) == 0
+    if (isThereAnExistingPullRequest) {
+        echo "There is already an existing PR with title: ${title}"
+        return
+    }
     sh "git push origin ${branch}"
     def fullText = title + "\n\n" + description
     writeFile(file: temporaryFile, text: fullText)
