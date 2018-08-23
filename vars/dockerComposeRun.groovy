6863b0c9a12bbb376a86efb5a8e32850c038199c
Giorgio Sironi <g.sironi@elifesciences.org>
2018-08-23 13:02:04 +0100

    Add returnStdout

--- vars/dockerComposeRun.groovy
+++ vars/dockerComposeRun.groovy
@@ -3,12 +3,15 @@ import DockerCompose
 def call(service, command, tag='latest')
 {
     try {
-        sh DockerCompose
-            .command('run', ['docker-compose.yml', 'docker-compose.ci.yml'])
-            .withEnvironment('IMAGE_TAG', tag)
-            .withArgument(service)
-            .withArgument(command)
-            .toString()
+        return sh(
+            script: DockerCompose
+                .command('run', ['docker-compose.yml', 'docker-compose.ci.yml'])
+                .withEnvironment('IMAGE_TAG', tag)
+                .withArgument(service)
+                .withArgument(command)
+                .toString(),
+            returnStdout: true
+        )
     } finally {
         sh DockerCompose
             .command('down', ['docker-compose.yml', 'docker-compose.ci.yml'])
