ffc6770234370a1b5059d932001d887ff35f349d
Daniel Ecer <de-code@users.noreply.github.com>
2018-09-21 17:26:59 +0100

    changed dockerComposeRun to not capture output

--- vars/dockerComposeRun.groovy
+++ vars/dockerComposeRun.groovy
@@ -3,15 +3,12 @@ import DockerCompose
 def call(service, command, tag='latest')
 {
     try {
-        return sh(
-            script: DockerCompose
-                .command('run', ['docker-compose.yml', 'docker-compose.ci.yml'])
-                .withEnvironment('IMAGE_TAG', tag)
-                .withArgument(service)
-                .withArgument(command)
-                .toString(),
-            returnStdout: true
-        )
+        sh DockerCompose
+            .command('run', ['docker-compose.yml', 'docker-compose.ci.yml'])
+            .withEnvironment('IMAGE_TAG', tag)
+            .withArgument(service)
+            .withArgument(command)
+            .toString()
     } finally {
         sh DockerCompose
             .command('down', ['docker-compose.yml', 'docker-compose.ci.yml'])
