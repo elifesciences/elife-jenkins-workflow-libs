64e768035f453c90b3e642a5189e6ce5fbfdc166
Giorgio Sironi <g.sironi@elifesciences.org>
2018-10-15 17:54:41 +0100

    Specify end2end in elifeEnd2endClean

--- vars/elifeEnd2endClean.groovy
+++ vars/elifeEnd2endClean.groovy
@@ -2,7 +2,7 @@ def call() {
     lock('spectrum') {
         elifeOnNode({
             sh "cd ${env.SPECTRUM_PREFIX}; ./checkout.sh ${revision}"
-            sh "cd ${env.SPECTRUM_PREFIX}; SPECTRUM_ENVIRONMENT=${environmentName} ./clean.sh"
+            sh "cd ${env.SPECTRUM_PREFIX}; SPECTRUM_ENVIRONMENT=end2end ./clean.sh"
         }, 'elife-libraries--spectrum')
     }
 }
