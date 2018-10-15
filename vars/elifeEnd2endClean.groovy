30d710cc7e086f279ac42dc52d7eb90301857d47
Giorgio Sironi <g.sironi@elifesciences.org>
2018-10-15 17:55:14 +0100

    Specify master revision in elifeEnd2endClean

--- vars/elifeEnd2endClean.groovy
+++ vars/elifeEnd2endClean.groovy
@@ -1,7 +1,7 @@
 def call() {
     lock('spectrum') {
         elifeOnNode({
-            sh "cd ${env.SPECTRUM_PREFIX}; ./checkout.sh ${revision}"
+            sh "cd ${env.SPECTRUM_PREFIX}; ./checkout.sh master"
             sh "cd ${env.SPECTRUM_PREFIX}; SPECTRUM_ENVIRONMENT=end2end ./clean.sh"
         }, 'elife-libraries--spectrum')
     }
