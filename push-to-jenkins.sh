2d53e2cd3e90059f61ab88bb9337e0e52b6ca40d
Giorgio Sironi <g.sironi@elifesciences.org>
2018-10-18 13:36:39 +0100

    Always add jenkins remote

--- push-to-jenkins.sh
+++ push-to-jenkins.sh
@@ -14,8 +14,8 @@ port="$3"
 # TODO: only add remote if not existing
 if git remote | grep '^jenkins$'; then
     git remote rm jenkins
-    git remote add jenkins "ssh://${username}@${host}:${port}/workflowLibs.git"
 fi
+git remote add jenkins "ssh://${username}@${host}:${port}/workflowLibs.git"
 commit=$(git rev-parse HEAD)
 git checkout master
 git reset --hard "${commit}"
