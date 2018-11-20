6b782a623247dd128818ddcd8c63e5703dca9ca9
Giorgio Sironi <g.sironi@elifesciences.org>
2018-11-20 12:34:19 +0000

    Log and re-raise in dockerComposeBuild

--- vars/dockerComposeBuild.groovy
+++ vars/dockerComposeBuild.groovy
@@ -3,10 +3,18 @@ import DockerCompose
 def call(tag='latest')
 {
     sh "docker-wait-daemon"
-    sh DockerCompose
-        .command('build', ['docker-compose.yml', 'docker-compose.ci.yml'])
-        // enable verbosity to debug frequent timeout failures
-        .withGeneralOption('verbose')
-        .withEnvironment('IMAGE_TAG', tag)
-        .toString()
+    try {
+        sh DockerCompose
+            .command('build', ['docker-compose.yml', 'docker-compose.ci.yml'])
+            // enable verbosity to debug frequent timeout failures
+            .withGeneralOption('verbose')
+            .withEnvironment('IMAGE_TAG', tag)
+            .toString()
+    } catch (Exception e) {
+        echo "Exception class: "
+        echo e.getClass()
+        echo "Exception message: "
+        echo e.getMessage()
+        throw e
+    }
 }
