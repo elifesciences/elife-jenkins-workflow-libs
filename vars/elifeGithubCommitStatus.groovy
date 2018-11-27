54ef629f6b0fb5226700448298bab3611f0ad300
Giorgio Sironi <g.sironi@elifesciences.org>
2018-11-27 14:42:47 +0000

    Allow a Map to be passed to elifeGithubCommitStatus

--- vars/elifeGithubCommitStatus.groovy
+++ vars/elifeGithubCommitStatus.groovy
@@ -1,10 +1,19 @@
 import Revision
 
-def call(commit, status, context, description='elifeGithubCommitStatus step', targetUrl='') {
-    def revision = new Revision(commit)
+def call(commitOrMap, status, context, description='elifeGithubCommitStatus step', targetUrl='') {
+    if (commitOrMap instanceof Map) {
+        commitSha = commitOrMap['commit']
+        status = commitOrMap['status']
+        context = commitOrMap['context']
+        description = commitOrMap['description']
+        targetUrl = commitOrMap['targetUrl']
+    } else {
+        commitSha = commitOrMap
+    }
+    def revision = new Revision(commitSha)
     if (revision.isBranch()) {
         echo "No commit status to update for branch ${revision.toString()}"
     } else {
-        sh "commit=${commit} status=${status} context=${context} description='${description}' target_url='${targetUrl}' /usr/local/jenkins-scripts/notify_github_commit_status.sh"
+        sh "commit=${commitSha} status=${status} context=${context} description='${description}' target_url='${targetUrl}' /usr/local/jenkins-scripts/notify_github_commit_status.sh"
     }
 }
