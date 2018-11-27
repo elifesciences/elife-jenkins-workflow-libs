1599cb35fd6fcfda1b68274f90cd27dfa8aaf8ac
Giorgio Sironi <g.sironi@elifesciences.org>
2018-11-27 14:24:40 +0000

    Rationalize name of commit status using hyphens

--- vars/builderSmokeTests.groovy
+++ vars/builderSmokeTests.groovy
@@ -1,7 +1,8 @@
 def call(stackname, folder, commitStatusName='smoke_tests') {
+    def stack = Stack.fromName(stackname)
     def String commit = elifeGitRevision()
     def smokeTestsCmd = "smoke-tests ${folder}"
     def smokeTests = withCommitStatus({
         builderCmd stackname, smokeTestsCmd
-    }, 'smoke_tests', commit)
+    }, "${stack.environment()}/smoke-tests", commit)
 }
