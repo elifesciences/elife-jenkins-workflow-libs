e9ec2f80060bece70b53221ceef2f6491d93e395
Giorgio Sironi <g.sironi@elifesciences.org>
2019-04-16 11:00:18 +0100

    Add commit status to dockerComposeBuild

--- vars/dockerComposeBuild.groovy
+++ vars/dockerComposeBuild.groovy
@@ -2,19 +2,21 @@ import DockerCompose
 
 def call(tag='latest')
 {
-    sh "docker-wait-daemon"
-    try {
-        sh DockerCompose
-            .command('build', ['docker-compose.yml', 'docker-compose.ci.yml'])
-            // enable verbosity to debug frequent timeout failures
-            .withGeneralOption('verbose')
-            .withEnvironment('IMAGE_TAG', tag)
-            .toString()
-    } catch (Exception e) {
-        echo "Exception class: "
-        echo e.getClass().toString()
-        echo "Exception message: "
-        echo e.getMessage()
-        throw e
-    }
+    withCommitStatus({
+        sh "docker-wait-daemon"
+        try {
+            sh DockerCompose
+                .command('build', ['docker-compose.yml', 'docker-compose.ci.yml'])
+                // enable verbosity to debug frequent timeout failures
+                .withGeneralOption('verbose')
+                .withEnvironment('IMAGE_TAG', tag)
+                .toString()
+        } catch (Exception e) {
+            echo "Exception class: "
+            echo e.getClass().toString()
+            echo "Exception message: "
+            echo e.getMessage()
+            throw e
+        }
+    }, 'build', tag)
 }
