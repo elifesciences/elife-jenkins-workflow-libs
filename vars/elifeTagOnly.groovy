dc64f434a5a476dbea1c77448c1230d6207c3b34
Giorgio Sironi <g.sironi@elifesciences.org>
2019-04-25 14:32:28 +0100

    Introduce elifeTagOnly

---
+++ vars/elifeTagOnly.groovy
@@ -0,0 +1,10 @@
+def call(Closure body) {
+    // env.TAG_NAME is only specified for multibranch projects
+    // when a tag is build rather than a branch or a pull request
+    // e.g. "0.0.1"
+    if (env.TAG_NAME != null) {
+        body(env.TAG_NAME)
+    } else {
+        echo "Skipped actions because not on a tag"
+    }
+}
