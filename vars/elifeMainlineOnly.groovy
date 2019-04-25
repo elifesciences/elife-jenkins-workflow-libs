97b009f466bf24b4edabc23d85ffd982c2e7f2bf
Giorgio Sironi <g.sironi@elifesciences.org>
2019-04-25 14:04:33 +0100

    Exclude tags from builds

--- vars/elifeMainlineOnly.groovy
+++ vars/elifeMainlineOnly.groovy
@@ -1,6 +1,8 @@
 def call(Closure body) {
     // env.BRANCH_NAME is only specified for multibranch projects
     // and by extension, organization folders
+    // env.TAG_NAME is also specified for tags, but this check already excludes them
+    // since they have env.BRANCH_NAME
     if (env.BRANCH_NAME == null) {
         body()
     } else {
