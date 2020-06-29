38c438f404a667ce48a131793ae47056f2d4f27f
Luke <lsh-0@users.noreply.github.com>
2020-06-29 12:20:47 +0930

    elifeNpmRelease.groovy, boilerplate error handling

--- vars/elifeNpmRelease.groovy
+++ vars/elifeNpmRelease.groovy
@@ -1,18 +1,43 @@
 def packageVersion() {
-    // TODO: return null on empty or error
-    return sh(script: "cat package.json | jq .version", returnStdout: true)
+    try {
+        return sh(script: "cat package.json | jq .version", returnStdout: true).trim()
+    } catch (Exception e) {
+        echo "Failed to find 'version' in package.json"
+        return null
+    }
 }
 
 def packageName() {
-    // TODO: return null on empty or error
-    return sh(script: "cat package.json | jq .name", returnStdout: true)
+    try {
+        return sh(script: "cat package.json | jq .name", returnStdout: true).trim()
+    } catch (Exception e) {
+        echo "Failed to find 'name' in package.json"
+        return null
+    }
 }
 
-def publishedVersions() {
-    // TODO: return an empty list if nothing has been published
-    // TODO: return null on error
-    results = sh(script: "npm show ${packageName()} version", returnStdout: true)
-    return results.split()
+def publishedVersions(pkgname) {
+    // do I need a try+catch when I'm capturing the retval?
+    retval = sh(script: "npm view ${pkgname} version", returnStatus=true).trim()
+    if (retval == 1) {
+        // package doesn't exist/nothing has been published
+        return []
+    }
+    else if (retval > 1) {
+        // unhandled error talking to npm
+        echo "Error attempting to list npm packages for ${pkgname}"
+        return null
+    }
+    try {
+        // now that we know versions exist
+        // run command *again* to capture the list of versions on stdout
+        // todo: can I captured stdout + retval in one go??
+        results = sh(script: "npm view ${pkgname} version", returnStdout: true).trim()
+        return results.split()
+    } catch (Exception e) {
+        echo "Error listing npm packages for ${pkgname}"
+        return null
+    }
 }
 
 def call() {
@@ -24,7 +49,7 @@ def call() {
     assert pkgname : "'name' in package.json not found"
    
     // if we're having problems talking to npm, die loudly
-    published = publishedVersions()
+    published = publishedVersions(pkgname)
     assert published : "error fetching list of published packages for '${pkgname}'"
 
     // if the package version is present in the list of published versions, do nothing
