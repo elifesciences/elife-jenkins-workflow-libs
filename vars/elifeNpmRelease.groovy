fb564d0c4e25a2844eea550f35b17c2b4a0468a9
LSH <lsh-0@users.noreply.github.com>
2020-07-02 14:11:40 +0930

    elifeNpmRelease (#31)
    adds elifeNpmRelease.groovy and elifeNpmRelease.txt

--- vars/elifeNpmRelease.groovy
+++ vars/elifeNpmRelease.groovy
@@ -1,42 +1,36 @@
-def packageVersion() {
+def command(cmd) {
+    sh(script: cmd, returnStatus: true)
+}
+
+def command_wstdout(cmd) {
+    sh(script: cmd, returnStdout: true).trim()
+}
+
+def packageAttr(attr) {
     try {
-        return sh(script: "cat package.json | jq -r .version ", returnStdout: true).trim()
+        command_wstdout "cat package.json | jq -r .${attr}"
     } catch (Exception e) {
-        echo "Failed to find 'version' in package.json"
-        return null
+        println "Failed to find '${attr}' in package.json"
     }
 }
 
-def packageName() {
-    try {
-        return sh(script: "cat package.json | jq -r .name", returnStdout: true).trim()
-    } catch (Exception e) {
-        echo "Failed to find 'name' in package.json"
-        return null
-    }
-}
+def packageVersion() { packageAttr "version" }
+
+def packageName() { packageAttr "name" }
 
 def publishedVersions(pkgname) {
-    // do I need a try+catch when I'm capturing the retval?
-    retval = sh(script: "npm view \"${pkgname}\" version", returnStatus: true)
-    if (retval == 1) {
-        // package doesn't exist/nothing has been published
+    retval = command "npm view \"${pkgname}\" version --loglevel silent"
+    if (retval != 0) {
+        println "Problem fetching releases for ${pkgname} (package may not exist)"
         return []
-    }
-    else if (retval > 1) {
-        // unhandled error talking to npm
-        echo "Error attempting to list npm packages for ${pkgname}"
-        return null
     }
     try {
         // now that we know versions exist
         // run command *again* to capture the list of versions on stdout
-        // todo: can I captured stdout + retval in one go??
-        results = sh(script: "npm view \"${pkgname}\" version", returnStdout: true).trim()
+        results = command_wstdout "npm view \"${pkgname}\" version"
         return results.split()
     } catch (Exception e) {
-        echo "Error listing npm packages for ${pkgname}"
-        return null
+        println "Error listing npm packages for ${pkgname}"
     }
 }
 
@@ -50,16 +44,22 @@ def call() {
    
     // if we're having problems talking to npm, die loudly
     published = publishedVersions(pkgname)
-    assert published : "error fetching list of published packages for '${pkgname}'"
+    assert published != null : "error fetching releases for '${pkgname}'"
 
     // if the package version is present in the list of published versions, do nothing
-    if (published.containsKey(pkgver)) {
+    if (published.contains(pkgver)) {
         println "Package '${pkgname}' has already published version '${pkgver}' on npm. Nothing to do."
         return
     }
+
+    retval = sh(script: "npm install", returnStatus: true)
+    assert retval == 0 : "failed to build package"
     
     // state at this point: this version of this package does not exist on npm
     
-    // TODO: where to pull token from?
-    sh "npm publish --dry-run"
+    withCredentials([string(credentialsId: 'npm-credentials', variable: 'NPM_TOKEN')]) {
+        command "echo \"//registry.npmjs.org/:_authToken=\${NPM_TOKEN}\" > .npmrc"
+        command "npm publish --access public"
+        assert retval == 0 : "failed to publish package '${pkgname}'"
+    }
 }
