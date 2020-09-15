abe7ca0e391954be3996a714cfe9c2fa7c5fbb2c
NuclearRedeye <j.summerfield@elifesciences.org>
2020-09-15 15:25:05 +0100

    fix: jenkins pipelines fail waiting package publish to packagist

--- vars/elifeWaitPackagist.groovy
+++ vars/elifeWaitPackagist.groovy
@@ -1,3 +1,3 @@
 def call(packageName, revision) {
-    sh "timeout 60s /usr/local/jenkins-scripts/wait_packagist.sh ${packageName} ${revision}"
+    sh "timeout 5m /usr/local/jenkins-scripts/wait_packagist.sh ${packageName} ${revision}"
 }
