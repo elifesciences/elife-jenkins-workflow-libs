45aa2e01ed89dd04f75311edb3df858604986876
Luke <lsh-0@users.noreply.github.com>
2021-05-14 15:40:04 +0930

    elifeGitTagRevision, bug fixes, more checks. extant tag no longer a failure case

--- vars/elifeGitTagRevision.groovy
+++ vars/elifeGitTagRevision.groovy
@@ -1,7 +1,16 @@
 def call(tag, revision = null) {
-    if(revision != null) {
-        sh(script: "git checkout ${revision}")
+    assert (tag != null) : "given tag was null."
+    tag = String.valueOf(tag).trim()
+    assert (tag != "") : "given tag was empty."
+    if (!revision) {
+        revision = elifeGitRevision()
     }
-    retval = sh(script:'git tag ${tag} && git push --tags', returnStatus: true)
+    retval = sh(script: "git tag | grep ${tag}", returnStatus: true)
+    if(retval == 0) {
+        echo("skipping tag and release: '${tag}' already exists")
+        return null
+    }
+    retval = sh(script: "git checkout master && git tag ${tag} ${revision} && git push origin --tags", returnStatus: true)
     assert retval == 0 : "failed to tag repository with '${tag}'"
+    return [tag, revision]
 }
