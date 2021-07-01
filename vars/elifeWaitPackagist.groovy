fed53c583fb6d019553fe10576d826b9d052d611
Joel Summerfield <52923935+NuclearRedeye@users.noreply.github.com>
2021-07-01 14:06:49 +0100

    fix: update packagist timeout to 10 minutes (#44)

--- vars/elifeWaitPackagist.groovy
+++ vars/elifeWaitPackagist.groovy
@@ -2,5 +2,5 @@ def call(packageName, revision) {
     // "The search index is updated every five minutes. 
     // It will index (or reindex) any package that has been crawled since the last time the search indexer ran."
     // - https://packagist.org/about
-    sh "timeout 5m /usr/local/jenkins-scripts/wait_packagist.sh ${packageName} ${revision}"
+    sh "timeout 10m /usr/local/jenkins-scripts/wait_packagist.sh ${packageName} ${revision}"
 }
