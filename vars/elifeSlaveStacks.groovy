486f303019904a4f1762b82b3eedda01b6de42f6
Luke <lsh-0@users.noreply.github.com>
2022-09-19 10:20:58 +0930

    typo

--- vars/elifeSlaveStacks.groovy
+++ vars/elifeSlaveStacks.groovy
@@ -3,6 +3,6 @@ def call() {
         'elife-libraries--ci',
         'elife-libraries--powerful3',
         'elife-libraries--spectrum',
-        'elife-libraries--load
+        'elife-libraries--load'
     ]
 }
