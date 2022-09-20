199dc61e864b81ebda13ee6730542ff69691af70
Luke <lsh-0@users.noreply.github.com>
2022-09-20 16:55:25 +0930

    adds 'builderRunTask'

---
+++ vars/builderRunTask.groovy
@@ -0,0 +1,35 @@
+def call(String task, String ... args) {
+
+    // we want to fail if the command fails, return the stdout if it succeeds.
+    // we can't have both unfortunately:
+    // - https://issues.jenkins.io/browse/JENKINS-44930
+    // one strategy is to send stdout to a file and then check the return code:
+    // def output_file = ".stdout.txt" // but this is no good.
+    // multiple sequential calls will overwrite this file, and multiple calls in 
+    // parallel and it becomes non-deterministic or worse, corrupt.
+    // so we'll generate a random string to create a unique file:
+    // - https://unix.stackexchange.com/questions/230673/how-to-generate-a-random-string#answer-230676
+    def random_string = sh(script:"tr -dc A-Za-z0-9 </dev/urandom | head -c 13", returnStdout:true)
+    def output_file = ".stdout.${random_string}.txt"
+
+    // "/path/to/bldr sometask"
+    def shellCmd = "${env.BUILDER_PATH}bldr ${task}"
+    if (args) {
+        // "/path/to/bldr sometask:somearg1,somearg2"
+        shellCmd += ":" + _escapeCmd(args.join(","))
+    }
+    // print stdout as it appears but also capture to file
+    // "/path/to/bldr sometask:somearg1 | tee .stdout.dwG76zKTGZeOI.txt"
+    shellCmd += " | tee " + output_file
+
+    rc = sh(script:shellCmd, returnStatus:true) as Integer
+    if (rc == 0) {
+        return readFile(output_file).trim()
+    }
+
+    def error_msg = "builder task '${task}' failed: ${rc}"
+    if (args) {
+        error_msg = "builder task '${task}' with args ('${args}') failed: ${rc}"
+    }
+    error(error_msg)
+}
