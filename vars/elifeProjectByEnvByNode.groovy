884607e6fde07bf59cae6d98e75a025a2ee6c364
Luke <lsh-0@users.noreply.github.com>
2022-10-21 15:00:13 +1030

    elifeProjectByEnvByNode, typo

--- vars/elifeProjectByEnvByNode.groovy
+++ vars/elifeProjectByEnvByNode.groovy
@@ -9,7 +9,7 @@ def call(project, env_csv, closure) {
                 for (int node = 1; node <= node_count; node++) {
                     // "project--prod--1"
                     stage("${stackname}--${node}", {
-                        builderRunTask("aws.ec2.start", stackname, node as String)
+                        builderRunTask("aws.ec2.start_node", stackname, node as String)
                         closure(project, env, node)
                     });
                 };
