d1833797e629004b611867a98b01757b427aac57
Luke <lsh-0@users.noreply.github.com>
2022-10-21 14:45:54 +1030

    builderRunTask, run task as bash, not sh

--- vars/builderRunTask.groovy
+++ vars/builderRunTask.groovy
@@ -19,12 +19,15 @@ def call(String task, String ... args) {
         shell_cmd += ":" + _escapeCmd(args.join(","))
     }
     // print stdout as it appears but also capture to file
-    // "set -o pipefail; /path/to/bldr sometask:somearg1 | tee .stdout.dwG76zKTGZeOI.txt"
-    shell_cmd = "set -o pipefail; " + shell_cmd + " | tee " + output_file
+    // "/path/to/bldr sometask:somearg1 | tee .stdout.dwG76zKTGZeOI.txt"
+    shell_cmd = shell_cmd + " | tee " + output_file
 
     // disable input. this should be set by the shell, but just in case
-    non_interactive = "BUILDER_NON_INTERACTIVE=1 "
-    shell_cmd = non_interactive + shell_cmd
+    //non_interactive = "BUILDER_NON_INTERACTIVE=1 "
+    //shell_cmd = non_interactive + shell_cmd
+
+    // run as bash
+    shell_cmd = "/bin/bash -c \"set -o pipefail; " + _escapeCmd(shell_cmd) + "\""
 
     rc = sh(script:shell_cmd, returnStatus:true) as Integer
     if (rc == 0) {
