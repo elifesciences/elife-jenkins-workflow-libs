4a4da80d4c62db2d2fd5e24d6eb64210e074426a
Luke <lsh-0@users.noreply.github.com>
2022-11-01 14:07:45 +1030

    elifeUpdateProjectByEnv, prevent variable shadowing

--- vars/elifeUpdateProjectByEnv.groovy
+++ vars/elifeUpdateProjectByEnv.groovy
@@ -1,5 +1,5 @@
-def call(String project, String envlist) {
-    elifeProjectByEnvByNode(project, envlist, { project, env, node ->
+def call(String project_name, String envlist) {
+    elifeProjectByEnvByNode(project_name, envlist, { project, env, node ->
         def stackname = "${project}--${env}"
         builderCmdNode(stackname, node, "sudo /usr/local/bin/daily-security-update");
         builderRunTask("aws.ec2.stop_node", stackname, node as String)
