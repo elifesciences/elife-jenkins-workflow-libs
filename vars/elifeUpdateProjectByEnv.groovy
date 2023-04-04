027447af6470c45bb1539a24ac2a72ebbb6ca8bf
Luke <lsh-0@users.noreply.github.com>
2023-04-04 14:37:46 +0930

    elifeUpdateProjectByEnv, special handling for updating elife-alfred--prod

--- vars/elifeUpdateProjectByEnv.groovy
+++ vars/elifeUpdateProjectByEnv.groovy
@@ -1,14 +1,23 @@
 def call(String project_name, String envlist) {
     elifeProjectByEnvByNode(project_name, envlist, { project, env, node ->
         def stackname = "${project}--${env}"
-        try {
-            builderCmdNode(stackname, node, "sudo /usr/local/bin/daily-security-update");
-        } catch (err) {
-            println("call to 'daily-security-update' script failed: " + err.toString())
-            println("ignoring and restarting instance.")
+        fn = {
+            try {
+                builderCmdNode(stackname, node, "sudo /usr/local/bin/daily-security-update");
+            } catch (err) {
+                println("call to 'daily-security-update' script failed: " + err.toString())
+                println("ignoring and restarting instance.")
+            }
+            builderRunTask("aws.ec2.stop_node", stackname, node as String)
+            builderStart(stackname)
+            builderCmdNode(stackname, node, "sudo /usr/local/bin/daily-system-update");
         }
-        builderRunTask("aws.ec2.stop_node", stackname, node as String)
-        builderStart(stackname)
-        builderCmdNode(stackname, node, "sudo /usr/local/bin/daily-system-update");
+
+        if (stackname == "elife-alfred--prod") {
+            // special handling for updating the machine this job is running on.
+            elifeOnNode({fn()}, "elife-libraries--ci")
+        } else {
+            fn()
+        }
     })
 };
