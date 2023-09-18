213f2263ff4b82fc1f5bf70870ee21fea6b8d4be
LSH <2142748+lsh-0@users.noreply.github.com>
2023-09-18 12:25:00 +0930

    newrelic removal (#57)

--- vars/elifeNewRelicEnsureApdex.groovy
+++
@@ -1,8 +0,0 @@
-def call(applicationId, minimum=0.85) {
-    def currentApdex = elifeNewRelicApdex(applicationId)
-    if (currentApdex < minimum) {
-        throw new Exception("New Relic APM apdex for ${applicationId} was required to be > ${minimum}, but it is ${currentApdex}")
-    } else {
-        echo "New Relic APM apdex for ${applicationId} was required to be > ${minimum}, it is ${currentApdex}"
-    }
-}
