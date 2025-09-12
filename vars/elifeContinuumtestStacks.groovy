bd718477150539d30060117a7443cb881cec9293
Scott Aubrey <scott@aubrey.org.uk>
2025-09-12 09:46:22 +0100

    Update elifeContinuumtestStacks.groovy

--- vars/elifeContinuumtestStacks.groovy
+++ vars/elifeContinuumtestStacks.groovy
@@ -1,19 +1,13 @@
 def call() {
     return [
         'annotations--continuumtest',
-        // 'api-gateway--continuumtest',
-        // 'digests--continuumtest',
         'elife-bot--continuumtest',
         'elife-dashboard--continuumtest',
-        'iiif--continuumtest',
         'journal--annotationstest',
         'journal--continuumtestpreview',
         'journal--continuumtest',
         'journal-cms--continuumtest',
         'lax--continuumtest',
-        //'observer--continuumtest',
         'profiles--continuumtest',
-        // 'recommendations--continuumtest',
-        // 'search--continuumtest'
     ]
 }
