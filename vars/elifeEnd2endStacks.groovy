11bb2fb3ff4c5bb1b250021aee87735d81b176c0
Scott Aubrey <scott@aubrey.org.uk>
2025-09-12 09:46:36 +0100

    Update elifeEnd2endStacks.groovy to remove iiif

--- vars/elifeEnd2endStacks.groovy
+++ vars/elifeEnd2endStacks.groovy
@@ -1,18 +1,13 @@
 def call() {
     return [
         'annotations--end2end',
-        // 'api-gateway--end2end',
         'bioprotocol--end2end',
-        // 'digests--end2end',
         'elife-bot--end2end',
         'elife-dashboard--end2end',
-        'iiif--end2end',
         'journal--end2end',
         'journal-cms--end2end',
         'lax--end2end',
         'observer--end2end',
         'profiles--end2end',
-        // 'recommendations--end2end',
-        // 'search--end2end'
     ]
 }
